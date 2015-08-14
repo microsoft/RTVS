799ed2053b6f0e9ebd71cd4e3c1768dda71ccd41
Alexander Sher <alsher@microsoft.com>
2015-08-14 14:19:34 -0700

    - Fix build - Move Support into R/Support

---
+++ src/R/Support/Impl/RD/ContentType/RdContentTypeDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.R.Support.RD.ContentTypes
+{
+    public sealed class RdContentTypeDefinition
+    {
+        public const string LanguageName = "RDoc";
+        public const string ContentType = "RDoc";
+        public const string FileExtension = ".rd";
+
+        /// <summary>
+        /// Exports the RD content type
+        /// </summary>
+        [Export(typeof(ContentTypeDefinition))]
+        [Name(RdContentTypeDefinition.ContentType)]
+        [BaseDefinition("text")]
+        public ContentTypeDefinition IRdContentType { get; set; }
+
+        /// <summary>
+        /// Exports the R file extension
+        /// </summary>
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(RdContentTypeDefinition.ContentType)]
+        [FileExtension(RdContentTypeDefinition.FileExtension)]
+        public FileExtensionToContentTypeDefinition IRdFileExtension { get; set; }
+    }
+}
