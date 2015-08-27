41caee38be2843b1e199e56c9663ec58f1147f9e
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-08-27 14:01:28 -0700

    Test: Verify RD keyword tables sorted

---
+++ src/R/Support/Test/RD/Tokens/VerifySortedTables.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Languages.Core.Test.Tokens;
+using Microsoft.R.Support.RD.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.R.Support.Test.RD.Tokens
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class VerifySortedRdTables : TokenizeTestBase<RdToken, RdTokenType>
+    {
+        [TestMethod]
+        public void VerifySortedRdBlockKeywords()
+        {
+            string[] array = new List<string>(RdBlockContentType._rKeywords).ToArray();
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(RdBlockContentType._rKeywords[i], array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void VerifySortedRdVerbatimKeywords()
+        {
+            string[] array = new List<string>(RdBlockContentType._verbatimKeywords).ToArray();
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(RdBlockContentType._verbatimKeywords[i], array[i]);
+            }
+        }
+    }
+}
