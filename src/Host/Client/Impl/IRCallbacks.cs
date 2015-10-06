771268f0360721179bbbd0a0567c34dd2b7c1d00
Pavel Minaev <int19h@gmail.com>
2015-10-05 18:17:36 -0700

    Fix #84: Evaluation doesn't work on non-waiting states of the loop
    Run nested event loops when awaiting evaluation result to correctly handle other events produced as a side effect of evaluation.
    
    Fix #104: REPL does not detect RHost.ext shutdown immediately
    
    Flow CancellationToken through all host callbacks, and cancel it if process terminates before the main loop completes. Use TPL DataFlow BufferBlock instead of AsyncConcurrentQueue for true async (non-locking) dequeueing in RSession. Detect and report cancellation in RSession when responding to ReadConsole.
    
    Reformat code to conform to the project formatting style in all affected files.

--- src/Host/Client/Impl/IRCallbacks.cs
+++ src/Host/Client/Impl/IRCallbacks.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Microsoft.R.Host.Client
-{
-    public interface IRCallbacks
-    {
+namespace Microsoft.R.Host.Client {
+    public interface IRCallbacks {
         Task Connected(string rVersion);
         Task Disconnected();
-        Task<string> ReadConsole(IReadOnlyCollection<IRContext> contexts, string prompt, string buf, int len, bool addToHistory);
-        Task WriteConsoleEx(IReadOnlyCollection<IRContext> contexts, string buf, OutputType otype);
-        Task ShowMessage(IReadOnlyCollection<IRContext> contexts, string s);
-        Task<YesNoCancel> YesNoCancel(IReadOnlyCollection<IRContext> contexts, string s);
-        Task Busy(IReadOnlyCollection<IRContext> contexts, bool which);
-        Task Evaluate(IReadOnlyCollection<IRContext> contexts, IRExpressionEvaluator evaluator);
-        Task PlotXaml(IReadOnlyCollection<IRContext> contexts, string xamlFilePath);
+        Task<string> ReadConsole(IReadOnlyCollection<IRContext> contexts, string prompt, string buf, int len, bool addToHistory, CancellationToken ct);
+        Task WriteConsoleEx(IReadOnlyCollection<IRContext> contexts, string buf, OutputType otype, CancellationToken ct);
+        Task ShowMessage(IReadOnlyCollection<IRContext> contexts, string s, CancellationToken ct);
+        Task<YesNoCancel> YesNoCancel(IReadOnlyCollection<IRContext> contexts, string s, CancellationToken ct);
+        Task Busy(IReadOnlyCollection<IRContext> contexts, bool which, CancellationToken ct);
+        Task Evaluate(IReadOnlyCollection<IRContext> contexts, IRExpressionEvaluator evaluator, CancellationToken ct);
+        Task PlotXaml(IReadOnlyCollection<IRContext> contexts, string xamlFilePath, CancellationToken ct);
     }
 }
