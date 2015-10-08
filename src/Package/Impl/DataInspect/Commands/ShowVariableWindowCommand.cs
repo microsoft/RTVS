458261abb28d4876083504b2e7bbdb43f7e10cd5
JP Ham <jeongham@microsoft.com>
2015-10-07 23:39:10 -0700

    Show VariableWindow
    - new ShowVariableWindowCommand
    - initializing R script in separate file

---
+++ src/Package/Impl/DataInspect/Commands/ShowVariableWindowCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.R.Package.Commands;
+using Microsoft.VisualStudio.R.Packages.R;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.R.Package.DataInspect.Commands
+{
+    internal sealed class ShowVariableWindowCommand : MenuCommand
+    {
+        public ShowVariableWindowCommand() :
+            base((sender, args) => new Handler().OnCommand(),
+                 new CommandID(RGuidList.RCmdSetGuid, RPackageCommandId.icmdShowVariableExplorerWindow))
+        {
+        }
+
+        class Handler
+        {
+            public void OnCommand()
+            {
+                ShowWindowPane(typeof(VariableWindowPane), true);
+            }
+
+            private void ShowWindowPane(Type windowType, bool focus)
+            {
+                var window = RPackage.Current.FindWindowPane(windowType, 0, true) as ToolWindowPane;
+                if (window != null)
+                {
+                    var frame = window.Frame as IVsWindowFrame;
+                    if (frame != null)
+                    {
+                        ErrorHandler.ThrowOnFailure(frame.Show());
+                    }
+                    if (focus)
+                    {
+                        var content = window.Content as System.Windows.UIElement;
+                        if (content != null)
+                        {
+                            content.Focus();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Package/Impl/DataInspect/DataInspect.R
@@ -0,0 +1,51 @@
+.rtvs.datainspect.eval_into <<- function(con, expr, env) {
+	obj <- eval(parse(text = expr), env);
+	repr <- capture.output(str(obj, list.len = 0))
+
+	cat('"name": ', file = con, sep = '');
+	dput(expr, file = con);
+	cat(',"class": "', file = con, sep = '');
+	cat(class(obj), file = con);
+	cat('"', file = con, sep = '');
+	cat(',"value": ', file = con, sep = '');
+	dput(repr[1], file = con);
+	cat(',"type": ', file = con, sep = '');
+	dput(typeof(obj), file = con);
+}
+.rtvs.datainspect.eval <<- function(expr, env) {
+	con <- textConnection(NULL, open = "w");
+	json <- "{}";
+	tryCatch({
+		cat('{', file = con, sep = '');
+		.rtvs.datainspect.eval_into(con, expr, env);
+		cat('}\n', file = con, sep = '');
+		json <- textConnectionValue(con);
+	}, finally = {
+		close(con);
+	});
+	cat(json);
+}
+.rtvs.datainspect.env_vars <<- function(env) {
+	con <- textConnection(NULL, open = "w");
+	json <- "{}";
+	tryCatch({
+		cat('[', file = con, sep = '');
+		is_first <- TRUE;
+		for (varname in ls(env)) {
+			if (is_first) {
+				is_first <- FALSE;
+			}
+			else {
+				cat(', ', file = con, sep = '');
+			}
+			cat('{', file = con, sep = '');
+			.rtvs.datainspect.eval_into(con, varname, env);
+			cat('}', file = con, sep = '');
+		}
+		cat(']\n', file = con, sep = '');
+		json <- textConnectionValue(con);
+	}, finally = {
+		close(con);
+	});
+	cat(json);
+}
