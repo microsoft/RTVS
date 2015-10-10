838f3b21f9894993af3052b3301327588376f6df
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-10-09 19:16:04 -0700

    Missing files

---
+++ src/Package/Impl/Repl/Commands/ReplTypingCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.R.Editor.Commands;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace Microsoft.VisualStudio.R.Package.Repl.Commands
+{
+    internal sealed class ReplTypingCommandHandler: RTypingCommandHandler
+    {
+        public ReplTypingCommandHandler(ITextView textView) :
+            base(textView)
+        {
+        }
+
+        protected override void HandleCompletion(char typedChar)
+        {
+            if(typedChar == '{')
+            {
+                return;
+            }
+
+            base.HandleCompletion(typedChar);
+        }
+    }
+}
