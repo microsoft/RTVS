1d1df80a2c427d9e5df7db73c79f3cffcb20ee2f
Alexander Sher <alsher@microsoft.com>
2015-10-28 15:18:10 -0700

    Fix formatting in Microsoft.VisualStudio.ProjectSystem.FileSystemMirroring

--- src/ProjectSystem/Impl/IO/IMsBuildFileSystemFilter.cs
+++ src/ProjectSystem/Impl/IO/IMsBuildFileSystemFilter.cs
@@ -1,11 +1,9 @@
 using System.IO;
 
-namespace Microsoft.VisualStudio.ProjectSystem.FileSystemMirroring.IO
-{
-	public interface IMsBuildFileSystemFilter
-	{
-		bool IsFileAllowed(string relativePath, FileAttributes attributes);
-		bool IsDirectoryAllowed(string relativePath, FileAttributes attributes);
-		void Seal();
-	}
+namespace Microsoft.VisualStudio.ProjectSystem.FileSystemMirroring.IO {
+    public interface IMsBuildFileSystemFilter {
+        bool IsFileAllowed(string relativePath, FileAttributes attributes);
+        bool IsDirectoryAllowed(string relativePath, FileAttributes attributes);
+        void Seal();
+    }
 }
