e7734ff92d72b457e8e216a1f9b44ac1bc4265fe
Mikhail Arkhipov <mikhaila@outlook.com>
2015-10-28 12:54:18 -0700

    Fix #325: Add error message to warn user that feedback .zip containing logs is too large

--- src/Package/Impl/Feedback/SendMailCommand.cs
+++ src/Package/Impl/Feedback/SendMailCommand.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 using Microsoft.Languages.Editor.Shell;
 using Microsoft.VisualStudio.R.Package.Commands;
 using Microsoft.VisualStudio.R.Package.Interop;
+using Microsoft.VisualStudio.R.Package.Logging;
 
 namespace Microsoft.VisualStudio.R.Package.Feedback {
     internal class SendMailCommand : PackageCommand {
@@ -14,14 +18,18 @@ namespace Microsoft.VisualStudio.R.Package.Feedback {
             if (attachmentFile != null) {
                 mail.AddAttachment(attachmentFile);
             }
-            int result = mail.SendMailPopup(subject, body);
-            if(result != 0) {
-                string err = string.Format(
-                    "Error sending e-mail: {0}.\nPlease send e-mail manually to rtvscore@microsoft.com\n"+
-                    "with attached RTVSLogs.zip file that can be found in\n"+
-                    "C:\\Users\\<USER_NAME>\\AppData\\Local\\Temp.", result);
 
-                EditorShell.Current.ShowErrorMessage(err);
+            int result = mail.SendMailPopup(subject, body);
+            if (result != 0) {
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendFormat(Resources.Error_CannotSendFeedback1, result, Enum.GetName(typeof(MapiErrorCode), result));
+                sb.Append(Environment.NewLine);
+                sb.Append(Environment.NewLine);
+                sb.AppendFormat(Resources.Error_CannotSendFeedback2, DiagnosticLogs.RtvsLogZipFile, Path.GetTempPath());
+
+                EditorShell.Current.ShowErrorMessage(sb.ToString());
+                Process.Start(Path.GetTempPath());
             }
         }
     }
--- src/Package/Impl/Interop/MailUtility.cs
+++ src/Package/Impl/Interop/MailUtility.cs
@@ -179,4 +179,79 @@ namespace Microsoft.VisualStudio.R.Package.Interop {
         public int eIDSize;
         public IntPtr entryID;
     }
+
+    // MAPI error codes
+    public enum MapiErrorCode {
+        /// <summary>
+        /// A recipient matched more than one of the recipient descriptor structures and MAPI_DIALOG was not set. No message was sent.
+        /// </summary>
+        MAPI_E_AMBIGUOUS_RECIPIENT = 21,
+
+        /// <summary>
+        /// The specified attachment was not found. No message was sent.
+        /// </summary>
+        MAPI_E_ATTACHMENT_NOT_FOUND = 11,
+
+        /// <summary>
+        /// The specified attachment could not be opened.No message was sent.
+        /// </summary>
+        MAPI_E_ATTACHMENT_OPEN_FAILURE = 12,
+
+        /// <summary>
+        /// The type of a recipient was not MAPI_TO, MAPI_CC, or MAPI_BCC. No message was sent.
+        /// </summary>
+        MAPI_E_BAD_RECIPTYPE = 15,
+
+        /// <summary>
+        /// One or more unspecified errors occurred. No message was sent.
+        /// </summary>
+        MAPI_E_FAILURE = 2,
+
+        /// <summary>
+        /// There was insufficient memory to proceed. No message was sent.
+        /// </summary>
+        MAPI_E_INSUFFICIENT_MEMORY = 5,
+        /// <summary>
+        /// One or more recipients were invalid or did not resolve to any address.
+        /// </summary>
+        MAPI_E_INVALID_RECIPS = 25,
+
+        /// <summary>
+        /// There was no default logon, and the user failed to log on successfully when the logon dialog box was displayed.No message was sent.
+        /// </summary>
+        MAPI_E_LOGIN_FAILURE = 3,
+
+        /// <summary>
+        /// The text in the message was too large.No message was sent.
+        /// </summary>
+        MAPI_E_TEXT_TOO_LARGE = 18,
+
+        /// <summary>
+        /// There were too many file attachments. No message was sent.
+        /// </summary>
+        MAPI_E_TOO_MANY_FILES = 9,
+
+        /// <summary>
+        /// There were too many recipients.No message was sent.
+        /// </summary>
+        MAPI_E_TOO_MANY_RECIPIENTS = 10,
+
+        /// <summary>
+        /// The MAPI_FORCE_UNICODE flag is specified and Unicode is not supported.
+        /// Note This value can be returned by MAPISendMailW only.
+        /// </summary>
+        MAPI_E_UNICODE_NOT_SUPPORTED = 27,
+
+        /// <summary>
+        /// A recipient did not appear in the address list.No message was sent.
+        /// </summary>
+        MAPI_E_UNKNOWN_RECIPIENT = 14,
+
+        /// <summary>
+        /// The user canceled one of the dialog boxes. No message was sent.
+        /// </summary>
+        MAPI_E_USER_ABORT = 1,
+
+        MAPI_SUCCESS = 0
+    }
 }
