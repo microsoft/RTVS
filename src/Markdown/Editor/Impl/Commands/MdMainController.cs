d3ba59e09f93076d0d5fc21867e9008f4655041b
Alexander Sher <alsher@microsoft.com>
2015-10-28 14:39:43 -0700

    Fix formatting in Microsoft.Markdown.Editor

--- src/Markdown/Editor/Impl/Commands/MdMainController.cs
+++ src/Markdown/Editor/Impl/Commands/MdMainController.cs
@@ -5,47 +5,38 @@ using Microsoft.Languages.Editor.Services;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 
-namespace Microsoft.Markdown.Editor.Commands
-{
+namespace Microsoft.Markdown.Editor.Commands {
     /// <summary>
     /// Main R editor command controller
     /// </summary>
-    public class MdMainController : ViewController
-    {
+    public class MdMainController : ViewController {
         public MdMainController(ITextView textView, ITextBuffer textBuffer)
-            : base(textView, textBuffer)
-        {
+            : base(textView, textBuffer) {
             ServiceManager.AddService<MdMainController>(this, textView);
         }
 
-        public static MdMainController Attach(ITextView textView, ITextBuffer textBuffer)
-        {
+        public static MdMainController Attach(ITextView textView, ITextBuffer textBuffer) {
             MdMainController controller = FromTextView(textView);
-            if (controller == null)
-            {
+            if (controller == null) {
                 controller = new MdMainController(textView, textBuffer);
             }
 
             return controller;
         }
 
-        public static MdMainController FromTextView(ITextView textView)
-        {
+        public static MdMainController FromTextView(ITextView textView) {
             return ServiceManager.GetService<MdMainController>(textView);
         }
 
-        public override CommandStatus Status(Guid group, int id)
-        {
+        public override CommandStatus Status(Guid group, int id) {
             return base.Status(group, id);
         }
 
         /// <summary>
         /// Disposes main controller and removes it from service manager.
         /// </summary>
-        protected override void Dispose(bool disposing)
-        {
-            if (TextView != null)
-            {
+        protected override void Dispose(bool disposing) {
+            if (TextView != null) {
                 ServiceManager.RemoveService<MdMainController>(TextView);
             }
 
