9be1c0dfb03fc2f4a7cca34f9c407d44f9b10385
Mikhail Arkhipov <mikhaila@outlook.com>
2015-10-29 16:03:02 -0700

    Change mail to mailto

--- src/Package/Impl/Feedback/SendFrownCommand.cs
+++ src/Package/Impl/Feedback/SendFrownCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
-using Microsoft.Languages.Editor.Shell;
-using Microsoft.VisualStudio.R.Package.Commands;
+﻿using Microsoft.VisualStudio.R.Package.Commands;
 using Microsoft.VisualStudio.R.Package.Logging;
 using Microsoft.VisualStudio.R.Packages.R;
 
@@ -11,15 +6,10 @@ namespace Microsoft.VisualStudio.R.Package.Feedback {
     internal sealed class SendFrownCommand : SendMailCommand {
         //TODO: localize
         private const string _disclaimer =
-"DISCLAIMER: Please be aware that the data contained in the attached logs contain " +
-"your command history as well as all output displayed in the R Interactive Windows, " +
-"and that by sending this email you are sharing this information with the RTVS team " +
-"to help us diagnose the problem you are experiencing. You can open the attached .zip " +
-"file in this email to inspect the contents if you have any concerns before sending the feedback.\r\n\r\n" +
-"Please briefly describe what you were doing that led to the issue if applicable.";
-
-        private const int _maxAttachmentSizeMb = 5;
-
+@"Please attach RTVSLogs.zip file that can be found in your user TEMP folder 
+and briefly describe what you were doing that led to the issue if applicable. 
+Please be aware that the data contained in the attached logs contain 
+your command history as well as all output displayed in the R Interactive Window";
 
         public SendFrownCommand() :
             base(RGuidList.RCmdSetGuid, RPackageCommandId.icmdSendFrown) {
@@ -32,27 +22,7 @@ namespace Microsoft.VisualStudio.R.Package.Feedback {
         protected override void Handle() {
 
             string zipPath = DiagnosticLogs.Collect();
-            if (!string.IsNullOrEmpty(zipPath) && File.Exists(zipPath)) {
-                if (new FileInfo(zipPath).Length > _maxAttachmentSizeMb * (1024 * 1024)) {
-                    StringBuilder sb = new StringBuilder();
-
-                    sb.AppendFormat(Resources.AttachmentTooLarge1, _maxAttachmentSizeMb);
-                    sb.Append(Environment.NewLine);
-                    sb.Append(Environment.NewLine);
-
-                    sb.Append(Resources.AttachmentTooLarge2);
-                    sb.Append(Environment.NewLine);
-                    sb.Append(Environment.NewLine);
-
-                    sb.AppendFormat(Resources.AttachmentTooLarge3, DiagnosticLogs.RtvsLogZipFile, Path.GetTempPath());
-
-                    EditorShell.Current.ShowErrorMessage(sb.ToString());
-                    Process.Start(Path.GetTempPath());
-                    zipPath = null;
-                }
-
-                SendMail(_disclaimer, "RTVS Frown", zipPath);
-            }
+            SendMail(_disclaimer, "RTVS Frown", zipPath);
         }
     }
 }
--- src/Package/Impl/Feedback/SendMailCommand.cs
+++ src/Package/Impl/Feedback/SendMailCommand.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
-using System.Text;
-using Microsoft.Languages.Editor.Shell;
 using Microsoft.VisualStudio.R.Package.Commands;
-using Microsoft.VisualStudio.R.Package.Interop;
-using Microsoft.VisualStudio.R.Package.Logging;
 
 namespace Microsoft.VisualStudio.R.Package.Feedback {
     internal class SendMailCommand : PackageCommand {
@@ -13,22 +10,12 @@ namespace Microsoft.VisualStudio.R.Package.Feedback {
             base(group, id) { }
 
         protected static void SendMail(string body, string subject, string attachmentFile) {
-            MapiMail mail = new MapiMail();
-            mail.AddRecipientTo("rtvscore@microsoft.com");
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = string.Format(CultureInfo.InvariantCulture, "mailto://rtvscore@microsoft.com?subject={0}&body={1}", subject, body);
+            Process.Start(psi);
+
             if (attachmentFile != null) {
-                mail.AddAttachment(attachmentFile);
-            }
-
-            int result = mail.SendMailPopup(subject, body);
-            if (result != 0) {
-                StringBuilder sb = new StringBuilder();
-
-                sb.AppendFormat(Resources.Error_CannotSendFeedback1, result, Enum.GetName(typeof(MapiErrorCode), result));
-                sb.Append(Environment.NewLine);
-                sb.Append(Environment.NewLine);
-                sb.AppendFormat(Resources.Error_CannotSendFeedback2, DiagnosticLogs.RtvsLogZipFile, Path.GetTempPath());
-
-                EditorShell.Current.ShowErrorMessage(sb.ToString());
                 Process.Start(Path.GetTempPath());
             }
         }
