42de235fbb7adad0f13557e01bb96b365d83f3c3
Pavel Minaev <int19h@gmail.com>
2015-10-29 09:45:07 -0700

    Fix parameter.

--- src/server.cpp
+++ src/server.cpp
@@ -326,7 +326,7 @@ namespace rhost {
                             // using longjmp, which will skip destructors for all our local variables. Instead, make
                             // CallBack a no-op until event processing is done, and then do a manual cancellation check.
                             allow_intr_in_CallBack = false;
-                            R_ToplevelExec([] {
+                            R_ToplevelExec([](void*) {
                                 // Errors can happen during event processing (from GUI windows such as graphs), and
                                 // we don't want them to bubble up here, so run these in a fresh execution context.
                                 R_WaitEvent();
