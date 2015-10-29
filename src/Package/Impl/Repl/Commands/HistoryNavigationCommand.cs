2ed956c95b343a8d0ffd11dedfa30d9c14a49b4c
dino <dinov@microsoft.com>
2015-10-28 19:33:14 -0700

    hoist current line to local, get editor ops once

--- src/Package/Impl/Repl/Commands/HistoryNavigationCommand.cs
+++ src/Package/Impl/Repl/Commands/HistoryNavigationCommand.cs
@@ -55,58 +55,67 @@ namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
                         id == (int)VSConstants.VSStd2KCmdID.RIGHT_EXT ||
                         id == (int)VSConstants.VSStd2KCmdID.LEFT_EXT;
 
+            var editorOps = _editorFactory.GetEditorOperations(TextView);
             if (curPoint != null) {
-                // history navigates if we're on the top or bottom line
-                if (id == (int)VSConstants.VSStd2KCmdID.UP) {
-                    if (curPoint.Value.GetContainingLine().LineNumber == 0) {
-                        // this leaves the caret at the end which is what we want for up/down to work nicely
-                        window.Operations.HistoryPrevious();
-                        return CommandResult.Executed;
-                    }
-                } else if (id == (int)VSConstants.VSStd2KCmdID.DOWN) {
-                    if (curPoint.Value.GetContainingLine().LineNumber == curPoint.Value.Snapshot.LineCount - 1) {
-                        window.Operations.HistoryNext();
-
-                        // move the caret to the 1st line in history so down/up works nicely
-                        var firstLine = window.CurrentLanguageBuffer.CurrentSnapshot.GetLineFromLineNumber(0);
-                        var upperPoint = MapUp(window, firstLine.Start);
-                        window.TextView.Caret.MoveTo(upperPoint.Value);
-                        return CommandResult.Executed;
-                    }
-                } else if (id == (int)VSConstants.VSStd2KCmdID.LEFT || id == (int)VSConstants.VSStd2KCmdID.LEFT_EXT) {
-                    if (curPoint.Value.GetContainingLine().Start == curPoint.Value) {
-                        if (curPoint.Value.GetContainingLine().LineNumber != 0) {
-                            // move to the end of the previous line rather then navigating into the prompts
-                            _editorFactory.GetEditorOperations(TextView).MoveLineUp(extend);
-                            window.Operations.End(extend);
+                var curLine = curPoint.Value.GetContainingLine();
+                switch ((VSConstants.VSStd2KCmdID)id) {
+                    // history navigates if we're on the top or bottom line
+                    case VSConstants.VSStd2KCmdID.UP:
+                        if (curLine.LineNumber == 0) {
+                            // this leaves the caret at the end which is what we want for up/down to work nicely
+                            window.Operations.HistoryPrevious();
+                            return CommandResult.Executed;
                         }
-                        return CommandResult.Executed;
+                        break;
+                    case VSConstants.VSStd2KCmdID.DOWN:
+                        if (curLine.LineNumber == curPoint.Value.Snapshot.LineCount - 1) {
+                            window.Operations.HistoryNext();
 
-                    }
-                } else if (id == (int)VSConstants.VSStd2KCmdID.RIGHT || id == (int)VSConstants.VSStd2KCmdID.RIGHT_EXT) {
-                    if (curPoint.Value.GetContainingLine().End == curPoint.Value) {
-                        if (curPoint.Value.GetContainingLine().LineNumber != curPoint.Value.Snapshot.LineCount - 1) {
-                            // move to the beginning of the next line rather then navigating into the prompts
-                            _editorFactory.GetEditorOperations(TextView).MoveLineDown(extend);
-
-                            curPoint = window.TextView.MapDownToBuffer(
-                                window.TextView.Caret.Position.BufferPosition,
-                                window.CurrentLanguageBuffer
-                            );
-                            Debug.Assert(curPoint != null);
-                            var start = curPoint.Value.GetContainingLine().Start;
-                            
-                            // Home would be nice here, but it goes to the beginning of the first non-whitespace char
-                            if (extend) {
-                                Text.VirtualSnapshotPoint anchor = TextView.Selection.AnchorPoint;
-                                TextView.Caret.MoveTo(MapUp(window, start).Value);
-                                TextView.Selection.Select(anchor.TranslateTo(TextView.TextSnapshot), TextView.Caret.Position.VirtualBufferPosition);
-                            } else {
-                                window.TextView.Caret.MoveTo(MapUp(window, start).Value);
+                            // move the caret to the 1st line in history so down/up works nicely
+                            var firstLine = window.CurrentLanguageBuffer.CurrentSnapshot.GetLineFromLineNumber(0);
+                            var upperPoint = MapUp(window, firstLine.Start);
+                            window.TextView.Caret.MoveTo(upperPoint.Value);
+                            return CommandResult.Executed;
+                        }
+                        break;
+                    case VSConstants.VSStd2KCmdID.LEFT:
+                    case VSConstants.VSStd2KCmdID.LEFT_EXT:
+                        if (curLine.Start == curPoint.Value) {
+                            if (curLine.LineNumber != 0) {
+                                // move to the end of the previous line rather then navigating into the prompts
+                                editorOps.MoveLineUp(extend);
+                                window.Operations.End(extend);
                             }
+                            return CommandResult.Executed;
+
                         }
-                        return CommandResult.Executed;
-                    }
+                        break;
+                    case VSConstants.VSStd2KCmdID.RIGHT:
+                    case VSConstants.VSStd2KCmdID.RIGHT_EXT:
+                        if (curLine.End == curPoint.Value) {
+                            if (curLine.LineNumber != curPoint.Value.Snapshot.LineCount - 1) {
+                                // move to the beginning of the next line rather then navigating into the prompts
+                                editorOps.MoveLineDown(extend);
+
+                                curPoint = window.TextView.MapDownToBuffer(
+                                    window.TextView.Caret.Position.BufferPosition,
+                                    window.CurrentLanguageBuffer
+                                );
+                                Debug.Assert(curPoint != null);
+                                var start = curPoint.Value.GetContainingLine().Start;
+
+                                // Home would be nice here, but it goes to the beginning of the first non-whitespace char
+                                if (extend) {
+                                    Text.VirtualSnapshotPoint anchor = TextView.Selection.AnchorPoint;
+                                    TextView.Caret.MoveTo(MapUp(window, start).Value);
+                                    TextView.Selection.Select(anchor.TranslateTo(TextView.TextSnapshot), TextView.Caret.Position.VirtualBufferPosition);
+                                } else {
+                                    window.TextView.Caret.MoveTo(MapUp(window, start).Value);
+                                }
+                            }
+                            return CommandResult.Executed;
+                        }
+                        break;
                 }
             }
 
@@ -115,19 +124,19 @@ namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
             switch ((VSConstants.VSStd2KCmdID)id) {
                 case VSConstants.VSStd2KCmdID.UP:
                 case VSConstants.VSStd2KCmdID.UP_EXT:
-                    _editorFactory.GetEditorOperations(TextView).MoveLineUp(extend);
+                    editorOps.MoveLineUp(extend);
                     break;
                 case VSConstants.VSStd2KCmdID.DOWN:
                 case VSConstants.VSStd2KCmdID.DOWN_EXT:
-                    _editorFactory.GetEditorOperations(TextView).MoveLineDown(extend);
+                    editorOps.MoveLineDown(extend);
                     break;
                 case VSConstants.VSStd2KCmdID.LEFT:
                 case VSConstants.VSStd2KCmdID.LEFT_EXT:
-                    _editorFactory.GetEditorOperations(TextView).MoveToPreviousCharacter(extend);
+                    editorOps.MoveToPreviousCharacter(extend);
                     break;
                 case VSConstants.VSStd2KCmdID.RIGHT:
                 case VSConstants.VSStd2KCmdID.RIGHT_EXT:
-                    _editorFactory.GetEditorOperations(TextView).MoveToNextCharacter(extend);
+                    editorOps.MoveToNextCharacter(extend);
                     break;
             }
 
