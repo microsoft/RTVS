458ef9de69ba4b8a54eedcdb3d3ded95ed0e0782
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-11-01 09:36:21 -0800

    Initial window sizing

--- src/Package/Impl/Plots/RPlotWindowContainer.cs
+++ src/Package/Impl/Plots/RPlotWindowContainer.cs
@@ -3,7 +3,10 @@ using System.Drawing;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using Microsoft.Languages.Editor.Shell;
+using Microsoft.Languages.Editor.Tasks;
 using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.R.Package.Shell;
 using Microsoft.VisualStudio.R.Package.Utilities;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -14,16 +17,12 @@ namespace Microsoft.VisualStudio.R.Package.Plots {
     /// </summary>
     class RPlotWindowContainer : UserControl, IVsWindowPane {
 
-        static Thread t;
-        static uint color1 = (uint)Color.FromArgb(255, 1, 1, 1).ToArgb();
-        static IntPtr _hdc = IntPtr.Zero;
+        private DateTime _lastActivationMessageTime = DateTime.Now;
+        private bool _connectedToIdle;
+        private bool _sized;
 
         #region IVsWindowPane
         public int ClosePane() {
-            if (_rPlotWindowHandle != IntPtr.Zero) {
-                //NativeMethods.PostMessage(_rPlotWindowHandle, NativeMethods.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
-                //_rPlotWindowHandle = IntPtr.Zero;
-            }
             return VSConstants.S_OK;
         }
 
@@ -40,16 +39,16 @@ namespace Microsoft.VisualStudio.R.Package.Plots {
 
             hwnd = this.Handle;
             NativeMethods.SetParent(this.Handle, hwndParent);
-            this.Show();
 
-            this.BackColor = System.Drawing.Color.Aqua;
+            // TODO: set watermark
+            SetBgColor();
+
+            this.Show();
             return VSConstants.S_OK;
         }
 
         public int GetDefaultSize(SIZE[] pSize) {
-            pSize[0].cx = 200;
-            pSize[0].cy = 400;
-            return VSConstants.S_OK;
+            return VSConstants.E_NOTIMPL;
         }
 
         public int LoadViewState(IStream pStream) {
@@ -93,18 +92,81 @@ namespace Microsoft.VisualStudio.R.Package.Plots {
         }
 
         protected override void OnClientSizeChanged(EventArgs e) {
-            IntPtr rPlotWindow = RPlotWindowHandle;
-            if (rPlotWindow != IntPtr.Zero) {
-                NativeMethods.MoveWindow(rPlotWindow, 0, 0, this.Width, this.Height, bRepaint: true);
-            }
+            SizeChildPlot(RPlotWindowHandle);
             base.OnClientSizeChanged(e);
         }
 
         protected override void WndProc(ref Message m) {
             if (m.Msg == NativeMethods.WM_ACTIVATE_PLOT) {
-                ToolWindowUtilities.ShowWindowPane<PlotWindowPane>(0, focus: false);
-            }
+                ForceSizeChildPlot();
+                _lastActivationMessageTime = DateTime.Now;
+                if (!_connectedToIdle) {
+                    _connectedToIdle = true;
+                    EditorShell.Current.Idle += OnIdle;
+                }
+            } else if (m.Msg == NativeMethods.WM_CLOSE) {
+                DestroyChildPlot();
+            } 
             base.WndProc(ref m);
+        }
+
+        private void OnIdle(object sender, EventArgs e) {
+            if ((DateTime.Now - _lastActivationMessageTime).TotalMilliseconds > 100) {
+                EditorShell.Current.Idle -= OnIdle;
+                _connectedToIdle = false;
+
+                PlotWindowPane pane = ToolWindowUtilities.FindWindowPane<PlotWindowPane>(0);
+                IVsWindowFrame frame = pane.Frame as IVsWindowFrame;
+                int onScreen = 0;
+                frame.IsOnScreen(out onScreen);
+                if (onScreen == 0) {
+                    ToolWindowUtilities.ShowWindowPane<PlotWindowPane>(0, focus: false);
+                    SizeChildPlot(IntPtr.Zero);
+                }
+            }
+        }
+
+        private void SizeChildPlot(IntPtr handle) {
+            handle = handle == IntPtr.Zero ? RPlotWindowHandle : handle;
+            if (handle != IntPtr.Zero) {
+                NativeMethods.MoveWindow(handle, 0, 0, this.Width, this.Height, bRepaint: true);
+            }
+        }
+
+        private void ForceSizeChildPlot() {
+            if (!_sized) {
+                IntPtr handle = RPlotWindowHandle;
+                if (handle != IntPtr.Zero) {
+                    NativeMethods.MoveWindow(handle, 0, 0, this.Width - 1, this.Height - 1, bRepaint: true);
+                    NativeMethods.MoveWindow(handle, 0, 0, this.Width, this.Height, bRepaint: true);
+                    _sized = true;
+                }
+            }
+        }
+
+        private void DestroyChildPlot() {
+            if (_rPlotWindowHandle != IntPtr.Zero) {
+                NativeMethods.PostMessage(_rPlotWindowHandle, NativeMethods.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                NativeMethods.DestroyWindow(_rPlotWindowHandle);
+                _rPlotWindowHandle = IntPtr.Zero;
+            }
+        }
+
+        protected override void Dispose(bool disposing) {
+            DestroyChildPlot();
+            base.Dispose(disposing);
+        }
+
+        protected override void OnParentBackColorChanged(EventArgs e) {
+            SetBgColor();
+            base.OnParentBackColorChanged(e);
+        }
+
+        private void SetBgColor() {
+            IVsUIShell2 uiShell = AppShell.Current.GetGlobalService<IVsUIShell2>(typeof(SVsUIShell));
+            uint color;
+            uiShell.GetVSSysColorEx((int)__VSSYSCOLOREX.VSCOLOR_TOOLWINDOW_BACKGROUND, out color);
+            this.BackColor = ColorTranslator.FromWin32((int)color);
         }
     }
 }
