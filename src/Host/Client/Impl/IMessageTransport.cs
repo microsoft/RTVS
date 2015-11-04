65a304fcf1ea9545f38e09656841e1667bf653c4
Pavel Minaev <pminaev@microsoft.com>
2015-11-04 10:03:05 -0800

    Move MessageTransportException to a separate source file.

--- src/Host/Client/Impl/IMessageTransport.cs
+++ src/Host/Client/Impl/IMessageTransport.cs
@@ -7,22 +7,4 @@ namespace Microsoft.R.Host.Client {
         Task SendAsync(string message, CancellationToken ct = default(CancellationToken));
         Task<string> ReceiveAsync(CancellationToken ct = default(CancellationToken));
     }
-
-    [Serializable]
-    public class MessageTransportException : Exception {
-        public MessageTransportException() {
-        }
-
-        public MessageTransportException(string message)
-            : base(message) {
-        }
-
-        public MessageTransportException(string message, Exception innerException)
-            : base(message, innerException) {
-        }
-
-        public MessageTransportException(Exception innerException)
-            : this(innerException.Message, innerException) {
-        }
-    }
 }
