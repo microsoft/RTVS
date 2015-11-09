bef078fe8683c61f175dbebaaba6e7e659e984f0
Pavel Minaev <pminaev@microsoft.com>
2015-11-09 14:39:46 -0800

    Add copyright notice header to source files.

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,3 +1,25 @@
+/* ****************************************************************************
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved. 
+ *
+ *
+ * This file is part of Microsoft R Host.
+ * 
+ * Microsoft R Host is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Microsoft R Host is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***************************************************************************/
+
 #include "eval.h"
 
 namespace rhost {
