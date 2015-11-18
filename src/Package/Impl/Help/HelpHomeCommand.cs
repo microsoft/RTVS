972e575adfb9fcb3dd5352085158d3c5b1ea4971
MikhailArkhipov <mikhaila@outlook.com>
2015-11-18 12:44:17 -0800

    Make standard R help page default

--- src/Package/Impl/Help/HelpHomeCommand.cs
+++ src/Package/Impl/Help/HelpHomeCommand.cs
@@ -1,30 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using Microsoft.Languages.Editor;
 using Microsoft.Languages.Editor.Controller.Command;
+using Microsoft.Languages.Editor.Shell;
+using Microsoft.R.Host.Client;
 using Microsoft.VisualStudio.R.Package.Commands;
 using Microsoft.VisualStudio.R.Packages.R;
 
 namespace Microsoft.VisualStudio.R.Package.Help {
     internal sealed class HelpHomeCommand : Command {
-        public const string HomeUrl = "http://rtvs";
-
-        private WebBrowser _browser;
-
         public HelpHomeCommand(WebBrowser browser) :
             base(new CommandId(RGuidList.RCmdSetGuid, RPackageCommandId.icmdHelpHome)) {
-            _browser = browser;
         }
 
         public override CommandStatus Status(Guid group, int id) {
-            return _browser != null ? CommandStatus.SupportedAndEnabled : CommandStatus.Supported;
+            return CommandStatus.SupportedAndEnabled;
         }
 
         public override CommandResult Invoke(Guid group, int id, object inputArg, ref object outputArg) {
-            if (_browser != null) {
-                _browser.Navigate(HelpHomeCommand.HomeUrl);
-            }
+            ShowDefaultHelpPage();
             return CommandResult.Executed;
+        }
+
+        public static void ShowDefaultHelpPage() {
+            Task.Run(async () => await ShowDefaultHelpPageAsync());
+        }
+
+        private static async Task ShowDefaultHelpPageAsync() {
+            var rSessionProvider = EditorShell.Current.ExportProvider.GetExportedValue<IRSessionProvider>();
+            IRSession session = rSessionProvider.Current;
+            if (session != null) {
+                try {
+                    using (IRSessionEvaluation evaluation = await session.BeginEvaluationAsync(isMutating: false)) {
+                        await evaluation.EvaluateAsync("help.start()" + Environment.NewLine);
+                    }
+                } catch (RException) { } catch (OperationCanceledException) { }
+            }
         }
     }
 }
