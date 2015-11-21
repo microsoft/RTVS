868cc6ec46fa6f0709324dbb16996fee323889f7
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-11-21 01:49:24 -0800

    Intellisense list filter tests

--- src/R/Editor/Application.Test/Completion/IntellisenseTest.cs
+++ src/R/Editor/Application.Test/Completion/IntellisenseTest.cs
@@ -51,5 +51,42 @@ namespace Microsoft.R.Editor.Application.Test.Completion {
                 Assert.AreEqual(expected, actual);
             }
         }
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        public void R_CompletionFilter01() {
+            using (var script = new TestScript(RContentTypeDefinition.ContentType)) {
+                script.Type("x <- lm");
+                script.DoIdle(100);
+                script.Type("mmm");
+                script.DoIdle(100);
+                script.Backspace();
+                script.Backspace();
+                script.Backspace();
+                script.Backspace();
+                script.DoIdle(100);
+                script.Type("abels.{TAB}");
+
+                string expected = "x <- labels.default";
+                string actual = script.EditorText;
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        public void R_CompletionFilter02() {
+            using (var script = new TestScript(RContentTypeDefinition.ContentType)) {
+                script.Type("x <- lm");
+                script.DoIdle(100);
+                script.Type("+");
+
+                string expected = "x <- lm+";
+                string actual = script.EditorText;
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
     }
 }
