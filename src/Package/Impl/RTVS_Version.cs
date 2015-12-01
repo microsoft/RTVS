6d15943ace2dcbaf801a8dd8aa6162b11f17a480
MikhailArkhipov <mikhaila@outlook.com>
2015-12-01 10:38:25 -0800

    Add basic version file

---
+++ src/Package/Impl/RTVS_Version.cs
@@ -0,0 +1,6 @@
+// Generated version file. Do not modify or check in.
+namespace Microsoft.VisualStudio.R.Package {
+    static class RTVS_Version {
+        public const string VersionString = "1.0.11201.1031";
+    }
+}
