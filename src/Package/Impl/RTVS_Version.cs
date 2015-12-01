c088ef03f6bac050754a65c94df359e59333d0f2
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-12-01 11:01:15 -0800

    Another ignore attempt

--- src/Package/Impl/RTVS_Version.cs
+++
@@ -1,6 +0,0 @@
-// Generated version file. Do not modify or check in.
-namespace Microsoft.VisualStudio.R.Package {
-    static class RTVS_Version {
-        public const string VersionString = "1.0.11201.1031";
-    }
-}
