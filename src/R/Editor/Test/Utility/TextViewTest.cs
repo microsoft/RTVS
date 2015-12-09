2881cf3b7b7f01164bf28e5a2514e35d61d176c7
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-12-08 18:36:31 -0800

    Test additions

--- src/R/Editor/Test/Utility/TextViewTest.cs
+++ src/R/Editor/Test/Utility/TextViewTest.cs
@@ -8,19 +8,25 @@ using Microsoft.R.Editor.ContentType;
 using Microsoft.VisualStudio.Editor.Mocks;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
 
-namespace Microsoft.R.Editor.Test.Utility
-{
+namespace Microsoft.R.Editor.Test.Utility {
     [ExcludeFromCodeCoverage]
-    public static class TextViewTest
-    {
-        public static ITextView MakeTextView(string content, out AstRoot ast)
-        {
+    public static class TextViewTest {
+        public static ITextView MakeTextView(string content, out AstRoot ast) {
             return MakeTextView(content, 0, out ast);
         }
 
-        public static ITextView MakeTextView(string content, int caretPosition, out AstRoot ast)
-        {
+        public static ITextView MakeTextViewRealTextBuffer(string content) {
+            EditorShell.SetShell(TestEditorShell.Create(REditorTestCompositionCatalog.Current));
+
+            ITextBufferFactoryService svc = EditorShell.Current.ExportProvider.GetExportedValue<ITextBufferFactoryService>();
+            IContentTypeRegistryService rg = EditorShell.Current.ExportProvider.GetExportedValue<IContentTypeRegistryService>();
+            ITextBuffer textBuffer = svc.CreateTextBuffer(content, rg.GetContentType(RContentTypeDefinition.ContentType));
+            return new TextViewMock(textBuffer, 0);
+        }
+
+        public static ITextView MakeTextView(string content, int caretPosition, out AstRoot ast) {
             EditorShell.SetShell(TestEditorShell.Create(REditorTestCompositionCatalog.Current));
 
             ast = RParser.Parse(content);
@@ -28,12 +34,11 @@ namespace Microsoft.R.Editor.Test.Utility
             return new TextViewMock(textBuffer, caretPosition);
         }
 
-        public static ITextView MakeTextView(string content, ITextRange selectionRange)
-        {
+        public static ITextView MakeTextView(string content, ITextRange selectionRange) {
             TextBufferMock textBuffer = new TextBufferMock(content, RContentTypeDefinition.ContentType);
             TextViewMock textView = new TextViewMock(textBuffer);
 
-            textView.Selection.Select(new SnapshotSpan(textBuffer.CurrentSnapshot, 
+            textView.Selection.Select(new SnapshotSpan(textBuffer.CurrentSnapshot,
                      new Span(selectionRange.Start, selectionRange.Length)), false);
             return textView;
         }
