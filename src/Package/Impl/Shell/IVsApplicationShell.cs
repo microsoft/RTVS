c24485fa7a8a847effe06bf47e3518daf8e465e1
MikhailArkhipov <mikhaila@outlook.com>
2015-12-09 16:21:49 -0800

    Missing files

---
+++ src/Package/Impl/Shell/IVsApplicationShell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Languages.Editor.Shell;
+
+namespace Microsoft.VisualStudio.R.Package.Shell {
+    /// <summary>
+    /// Application shell provides access to services such as 
+    /// composition container, export provider, global VS IDE
+    /// services and so on.
+    /// </summary>
+    public interface IVsApplicationShell : IEditorShell {
+        string BrowseForFileOpen(IntPtr owner, string filter, string initialPath = null, string title = null);
+
+        string BrowseForFileSave(IntPtr owner, string filter, string initialPath = null, string title = null);
+    }
+}
---
+++ src/Package/Impl/Shell/VsAppShell.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Common.Core;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.R.Package.Shell {
+    /// <summary>
+    /// Application shell provides access to services
+    /// such as composition container, export provider, global VS IDE
+    /// services and so on.
+    /// </summary>
+    public class VsAppShell : VsEditorShell, IVsApplicationShell {
+        private static Lazy<VsAppShell> _instance = Lazy.Create(() => new VsAppShell());
+        private static IVsApplicationShell _testShell;
+
+        /// <summary>
+        /// Current application shell instance. Provides access to services
+        /// such as composition container, export provider, global VS IDE
+        /// services and so on.
+        /// </summary>
+        public static IVsApplicationShell Current {
+            get { return _testShell ?? _instance.Value; }
+            // Only used in tests
+            internal set { _testShell = value; }
+        }
+
+        public VsAppShell() {
+            // Check if test assemblies are loaded into the VS process
+            this.DetemineTestEnvironment();
+        }
+
+        #region IVSApplicationShell
+        public string BrowseForFileOpen(IntPtr owner, string filter, string initialPath = null, string title = null) {
+            IVsUIShell uiShell = VsAppShell.Current.GetGlobalService<IVsUIShell>(typeof(SVsUIShell));
+            if (uiShell == null) {
+                return null;
+            }
+
+            if (owner == IntPtr.Zero) {
+                ErrorHandler.ThrowOnFailure(uiShell.GetDialogOwnerHwnd(out owner));
+            }
+
+            VSOPENFILENAMEW[] openInfo = new VSOPENFILENAMEW[1];
+            openInfo[0].lStructSize = (uint)Marshal.SizeOf(typeof(VSOPENFILENAMEW));
+            openInfo[0].pwzFilter = filter.Replace('|', '\0') + "\0";
+            openInfo[0].hwndOwner = owner;
+            openInfo[0].pwzDlgTitle = title;
+            openInfo[0].nMaxFileName = 260;
+            var pFileName = Marshal.AllocCoTaskMem(520);
+            openInfo[0].pwzFileName = pFileName;
+            openInfo[0].pwzInitialDir = Path.GetDirectoryName(initialPath);
+            var nameArray = (Path.GetFileName(initialPath) + "\0").ToCharArray();
+            Marshal.Copy(nameArray, 0, pFileName, nameArray.Length);
+
+            try {
+                int hr = uiShell.GetOpenFileNameViaDlg(openInfo);
+                if (hr == VSConstants.OLE_E_PROMPTSAVECANCELLED) {
+                    return null;
+                }
+                ErrorHandler.ThrowOnFailure(hr);
+                return Marshal.PtrToStringAuto(openInfo[0].pwzFileName);
+            } finally {
+                if (pFileName != IntPtr.Zero) {
+                    Marshal.FreeCoTaskMem(pFileName);
+                }
+            }
+        }
+
+        public string BrowseForFileSave(IntPtr owner, string filter, string initialPath = null, string title = null) {
+            if (string.IsNullOrEmpty(initialPath)) {
+                initialPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + Path.DirectorySeparatorChar;
+            }
+
+            IVsUIShell uiShell = VsAppShell.Current.GetGlobalService<IVsUIShell>(typeof(SVsUIShell));
+            if (null == uiShell) {
+                return null;
+            }
+
+            if (owner == IntPtr.Zero) {
+                ErrorHandler.ThrowOnFailure(uiShell.GetDialogOwnerHwnd(out owner));
+            }
+
+            VSSAVEFILENAMEW[] saveInfo = new VSSAVEFILENAMEW[1];
+            saveInfo[0].lStructSize = (uint)Marshal.SizeOf(typeof(VSSAVEFILENAMEW));
+            saveInfo[0].pwzFilter = filter.Replace('|', '\0') + "\0";
+            saveInfo[0].hwndOwner = owner;
+            saveInfo[0].pwzDlgTitle = title;
+            saveInfo[0].nMaxFileName = 260;
+            var pFileName = Marshal.AllocCoTaskMem(520);
+            saveInfo[0].pwzFileName = pFileName;
+            saveInfo[0].pwzInitialDir = Path.GetDirectoryName(initialPath);
+            var nameArray = (Path.GetFileName(initialPath) + "\0").ToCharArray();
+            Marshal.Copy(nameArray, 0, pFileName, nameArray.Length);
+            try {
+                int hr = uiShell.GetSaveFileNameViaDlg(saveInfo);
+                if (hr == VSConstants.OLE_E_PROMPTSAVECANCELLED) {
+                    return null;
+                }
+                ErrorHandler.ThrowOnFailure(hr);
+                return Marshal.PtrToStringAuto(saveInfo[0].pwzFileName);
+            } finally {
+                if (pFileName != IntPtr.Zero) {
+                    Marshal.FreeCoTaskMem(pFileName);
+                }
+            }
+        }
+        #endregion
+
+        // Check if test assemblies are loaded into the VS process
+        private void DetemineTestEnvironment() {
+            AppDomain ad = AppDomain.CurrentDomain;
+            Assembly[] loadedAssemblies = ad.GetAssemblies();
+
+            Assembly testAssembly = loadedAssemblies.FirstOrDefault((asm) => {
+                AssemblyName assemblyName = asm.GetName();
+                string name = assemblyName.Name;
+                return name.IndexOf("apex", StringComparison.OrdinalIgnoreCase) >= 0 || name.IndexOf(".mocks", StringComparison.OrdinalIgnoreCase) >= 0;
+            });
+
+            this.IsUITestEnvironment = testAssembly != null;
+        }
+    }
+}
