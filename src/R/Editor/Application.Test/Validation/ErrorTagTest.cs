7d650e769e46fec9e96d56338f8b1d301a761375
MikhailArkhipov <mikhaila@outlook.com>
2015-12-16 18:28:08 -0800

    Basic error squiggly test

---
+++ src/R/Editor/Application.Test/Validation/ErrorTagTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.R.Editor.Application.Test.TestShell;
+using Microsoft.R.Editor.ContentType;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.R.Editor.Application.Test.Validation {
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ErrorTagTest {
+        [TestMethod]
+        [TestCategory("Interactive")]
+        public void R_ErrorTagsTest01() {
+            using (var script = new TestScript(RContentTypeDefinition.ContentType)) {
+                // Force tagger creation
+                var tagSpans = script.GetErrorTagSpans();
+
+                script.Type("x <- {");
+                script.Delete();
+                script.DoIdle(500);
+
+                tagSpans = script.GetErrorTagSpans();
+                string errorTags = script.WriteErrorTags(tagSpans);
+                Assert.AreEqual("[5 - 6] } expected\r\n", errorTags);
+            }
+        }
+    }
+}
