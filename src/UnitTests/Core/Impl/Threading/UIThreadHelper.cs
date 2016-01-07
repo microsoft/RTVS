42ed76670e578871aadb901960010cbfb1c49297
Alexander Sher <alsher@microsoft.com>
2016-01-06 16:40:07 -0800

    Fix RCW leaks in UI Thread created by UIThreadHelper With heavy UI controls, it is possible that finalizer thread started by unloading AppDomain will trigger ShutDownEvents created by these controls before Dispatcher is shut down, so we should pause the finalizer thread and wait until UI thread is terminated.

--- src/UnitTests/Core/Impl/Threading/UIThreadHelper.cs
+++ src/UnitTests/Core/Impl/Threading/UIThreadHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
@@ -128,6 +127,11 @@ namespace Microsoft.UnitTests.Core.Threading
                 }
             }
 
+            var dispatcher = Dispatcher.FromThread(Thread.CurrentThread);
+            if (dispatcher != null && !dispatcher.HasShutdownStarted) {
+                dispatcher.InvokeShutdown();
+            }
+
             if (exceptionInfos.Any())
             {
                 throw new AggregateException(exceptionInfos.Select(ce => ce.SourceException).ToArray());
@@ -144,10 +148,8 @@ namespace Microsoft.UnitTests.Core.Threading
             _frame.Continue = false;
 
             // If the thread is still alive, allow it to exit normally so the dispatcher can continue to clear pending work items
-            if (mainThread.IsAlive)
-            {
-                Debug.WriteLine("Thread is alive, trying to wait.");
-            }
+            // 10 seconds should be enough
+            mainThread.Join(10000);
         }
 
         private static ExceptionDispatchInfo CallSafe(Action action)
