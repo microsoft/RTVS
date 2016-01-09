7b5f210ebe14049d7ef537cfd22f43b75b9c0a3c
Mikhail Arkhipov <mikhaila@outlook.com>
2016-01-08 23:59:50 -0800

    Fix window layout Add missing commands to menu Fix outlining menu

--- src/Package/Impl/Packages/R/RPackage.cs
+++ src/Package/Impl/Packages/R/RPackage.cs
@@ -39,7 +39,7 @@ namespace Microsoft.VisualStudio.R.Packages.R {
     [ProvideEditorFactory(typeof(REditorFactory), 106, CommonPhysicalViewAttributes = 0x2, TrustLevel = __VSEDITORTRUSTLEVEL.ETL_AlwaysTrusted)]
     [ProvideEditorLogicalView(typeof(REditorFactory), VSConstants.LOGVIEWID.TextView_string)]
     [ProvideLanguageService(typeof(RLanguageService), RContentTypeDefinition.LanguageName, 106, ShowSmartIndent = true,
-        ShowMatchingBrace = true, MatchBraces = true, MatchBracesAtCaret = true, ShowCompletion = true,
+        ShowMatchingBrace = true, MatchBraces = true, MatchBracesAtCaret = true, ShowCompletion = true, EnableLineNumbers = true,
         EnableFormatSelection = true, DefaultToInsertSpaces = true, RequestStockColors = true)]
     [ShowBraceCompletion(RContentTypeDefinition.LanguageName)]
     [DefaultIndentAttribute(RContentTypeDefinition.LanguageName, 2, true)]
--- src/Package/Impl/Repl/Workspace/RestartRCommand.cs
+++ src/Package/Impl/Repl/Workspace/RestartRCommand.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.VisualStudio.R.Package.Commands;
+﻿using System;
+using Microsoft.VisualStudio.R.Package.Commands;
+using Microsoft.VisualStudio.R.Package.Shell;
+using Microsoft.VisualStudio.R.Packages.R;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Microsoft.VisualStudio.R.Package.Repl.Workspace {
     internal sealed class RestartRCommand : PackageCommand {
         public RestartRCommand() :
-            base(VSConstants.VsStd11, (int)VSConstants.VSStd11CmdID.InteractiveSessionRestart) {
+            base(RGuidList.RCmdSetGuid, RPackageCommandId.icmdRestartR) {
         }
 
         protected override void SetStatus() {
@@ -13,6 +17,13 @@ namespace Microsoft.VisualStudio.R.Package.Repl.Workspace {
             } else {
                 Visible = false;
             }
+        }
+
+        protected override void Handle() {
+            IVsUIShell shell = VsAppShell.Current.GetGlobalService<IVsUIShell>(typeof(SVsUIShell));
+            Guid group = VSConstants.VsStd11;
+            object o = null;
+            shell.PostExecCommand(ref group, (uint)VSConstants.VSStd11CmdID.InteractiveSessionRestart, 0, ref o);
         }
     }
 }
