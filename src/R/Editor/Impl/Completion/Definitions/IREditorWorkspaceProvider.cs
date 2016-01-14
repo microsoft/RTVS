d89563356301d0b568ad4f7cb947833b0627c6f5
Mikhail Arkhipov <mikhaila@microsoft.com>
2016-01-13 17:15:49 -0800

    Missing file

---
+++ src/R/Editor/Impl/Completion/Definitions/IREditorWorkspaceProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.R.Editor.Completion.Definitions {
+    /// <summary>
+    /// Represents R workspace to the editor
+    /// </summary>
+    public interface IREditorWorkspaceProvider {
+        IREditorWorkspace GetWorkspace();
+    }
+}
