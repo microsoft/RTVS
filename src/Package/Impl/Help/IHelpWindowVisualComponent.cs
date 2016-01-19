2d9a882e0fd6e6b2d5541fe8eec847f6d310a9e3
MikhailArkhipov <mikhaila@outlook.com>
2016-01-19 13:15:47 -0800

    Make interfaces public for signed builds

--- src/Package/Impl/Help/IHelpWindowVisualComponent.cs
+++ src/Package/Impl/Help/IHelpWindowVisualComponent.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.R.Package.Definitions;
 
 namespace Microsoft.VisualStudio.R.Package.Help {
-    internal interface IHelpWindowVisualComponent : IVisualComponent {
+    public interface IHelpWindowVisualComponent : IVisualComponent {
         /// <summary>
         /// Browser that displays help content
         /// </summary>
