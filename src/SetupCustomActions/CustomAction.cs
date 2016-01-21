1795f19e5741e5a423db4de5b740af468425809b
Mikhail Arkhipov <mikhaila@outlook.com>
2016-01-20 22:56:12 -0800

    Setup custom action

---
+++ src/SetupCustomActions/CustomAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace SetupCustomActions {
+    public class CustomActions {
+        [CustomAction]
+        public static ActionResult DSProfilePromptAction(Session session) {
+            session.Log("Begin DSProfilePromptAction");
+            MessageBox.Show("Would you like to set Data Scientist profile?", "R Tools for Visual Studio");
+            return ActionResult.Success;
+        }
+    }
+}
