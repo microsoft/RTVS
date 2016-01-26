67019959828584c811d300adf8aaeceb23ca5373
JeongPyoHam <jeongham@microsoft.com>
2016-01-26 10:31:13 -0800

    Delete unused file

--- src/Package/Impl/DataInspect/VariableChangedArgs.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Microsoft.VisualStudio.R.Package.DataInspect {
-    internal class VariableChangedArgs : EventArgs {
-        public EvaluationWrapper NewVariable { get; set; }
-    }
-}
