b39677fe20ca711d52b8a786302ebc01b22070f8
Pavel Minaev <int19h@gmail.com>
2016-01-28 06:11:16 -0800

    Fix  #672: When debugging at global scope, we see lots of RTVS internal functions showing up in Locals
    Make hiding dot-prefixed variables an option, and expose it in the context menu for Autos/Locals/Watch tool window.

---
+++ src/Package/Impl/Debugger/Commands/ShowDotPrefixedVariablesCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EnvDTE;
+using Microsoft.R.Debugger.Engine;
+using Microsoft.R.Editor.ContentType;
+using Microsoft.R.Support.Settings;
+using Microsoft.R.Support.Settings.Definitions;
+using Microsoft.VisualStudio.R.Package.Commands;
+using Microsoft.VisualStudio.R.Package.Shell;
+using Microsoft.VisualStudio.R.Packages.R;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace Microsoft.VisualStudio.R.Package.Debugger.Commands {
+    internal class ShowDotPrefixedVariablesCommand : PackageCommand {
+        private readonly IRToolsSettings _settings;
+
+        public ShowDotPrefixedVariablesCommand(IRToolsSettings settings)
+            : base(RGuidList.RCmdSetGuid, RPackageCommandId.icmdShowDotPrefixedVariables) {
+            _settings = settings;
+        }
+
+
+        protected override void SetStatus() {
+            Checked = _settings.ShowDotPrefixedVariables;
+
+            // Only show it in the debugger context menu when debugging R code to avoid clutter.
+            var debugger = VsAppShell.Current.GetGlobalService<DTE>().Debugger;
+            Enabled = Visible = debugger.CurrentStackFrame?.Language == RContentTypeDefinition.LanguageName;
+        }
+
+        protected override void Handle() {
+           _settings.ShowDotPrefixedVariables = !_settings.ShowDotPrefixedVariables;
+
+            // A hackish way to force debugger to refresh its views, so that our EE is requeried and can use the new option value.
+            var debugger = VsAppShell.Current.GetGlobalService<DTE>().Debugger;
+            debugger.HexDisplayMode = debugger.HexDisplayMode;
+        }
+    }
+}
