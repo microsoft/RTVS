04f3a6948d6ab658ba533b29cc2e0718405986e3
Mikhail Arkhipov <mikhaila@outlook.com>
2016-01-30 12:13:25 -0800

    Enable command factory test

--- src/Package/Test/Commands/CommandFactoryTest.cs
+++ src/Package/Test/Commands/CommandFactoryTest.cs
@@ -5,18 +5,23 @@ using Microsoft.Languages.Editor.Composition;
 using Microsoft.Languages.Editor.Controller;
 using Microsoft.Languages.Editor.Shell;
 using Microsoft.R.Editor.ContentType;
+using Microsoft.UnitTests.Core.XUnit;
+using Microsoft.VisualStudio.R.Package.Shell;
 
 namespace Microsoft.VisualStudio.R.Package.Test.Commands {
     [ExcludeFromCodeCoverage]
     public class CommandFactoryTest
     {
-        //[Test]
-        //[Category.R.Package]
-        public void Package_CommandFactoryImportTest()
+        [Test]
+        [Category.R.Package]
+        public void CommandFactoryImportTest()
         {
-             var importComposer = new ContentTypeImportComposer<ICommandFactory>(EditorShell.Current.CompositionService);
+            var importComposer = new ContentTypeImportComposer<ICommandFactory>(EditorShell.Current.CompositionService);
             ICollection<ICommandFactory> factories = importComposer.GetAll(RContentTypeDefinition.ContentType);
+            factories.Should().HaveCount(1);
 
+            importComposer = new ContentTypeImportComposer<ICommandFactory>(VsAppShell.Current.CompositionService);
+            factories = importComposer.GetAll(RContentTypeDefinition.ContentType);
             factories.Should().HaveCount(2);
         }
     }
