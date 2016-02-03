07c7d822068e0d826b3898a5085e5538dec1e864
JeongPyoHam <jeongham@microsoft.com>
2016-02-03 09:25:17 -0800

    One file per definition

---
+++ src/Package/Impl/DataInspect/VisualGrid/ScrollDirection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Microsoft.VisualStudio.R.Package.DataInspect {
+    /// <summary>
+    /// Scroll orientation of Grid
+    /// </summary>
+    [Flags]
+    internal enum ScrollDirection {
+        /// <summary>
+        /// grid doesn't scroll
+        /// </summary>
+        None = 0x00,
+
+        /// <summary>
+        /// grid scrolls horizontally
+        /// </summary>
+        Horizontal = 0x01,
+
+        /// <summary>
+        /// grid scrolls vertically
+        /// </summary>
+        Vertical = 0x02,
+
+        /// <summary>
+        /// grid scrolls in vertical and horizontal direction
+        /// </summary>
+        Both = 0x03,
+    }
+}
