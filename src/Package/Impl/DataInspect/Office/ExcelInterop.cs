284d05c482f3039ddb92a0e6a168d934cebb8f35
MikhailArkhipov <mikhaila@microsoft.com>
2016-02-04 08:16:05 -0800

    Reuse existing sheet

--- src/Package/Impl/DataInspect/Office/ExcelInterop.cs
+++ src/Package/Impl/DataInspect/Office/ExcelInterop.cs
@@ -11,6 +11,12 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect.Office {
         private static Application _excel;
         private static Timer _timer;
 
+        class ExcelData {
+            public object[] RowNames;
+            public object[] ColNames;
+            public object[,] CellData;
+        }
+
         public static async Task OpenDataInExcel(string expression, int rows, int cols) {
             if (_busy) {
                 return;
@@ -18,60 +24,76 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect.Office {
 
             _busy = true;
 
-            object[] rowNames = null;
-            object[] colNames = null;
-
-            IGridData<string> data = await GridDataSource.GetGridDataAsync(expression,
-                                                new GridRange(new Range(0, rows), new Range(0, cols)));
-            if (data.RowHeader != null) {
-                rowNames = await MakeRowNames(data, rows);
-            }
-
-            if (data.RowHeader != null) {
-                colNames = await MakeColumnNames(data, cols);
-            }
-
-            object[,] arr = await MakeExcelRange(data, rows, cols);
-
+            ExcelData xlData = await GenerateExcelData(expression, rows, cols);
             try {
                 Workbook wb = RunExcel();
-                if (wb == null) {
-                    return;
-                }
+                if (wb != null) {
+                    // Try finding existing worksheet first
+                    Worksheet ws = null;
+                    try {
+                        ws = (Worksheet)wb.Sheets[expression];
+                    }
+                    catch (COMException) { }
 
-                Worksheet ws = (Worksheet)wb.Sheets.Add();
-
-                int startRow = colNames != null ? 2 : 1;
-                int startCol = rowNames != null ? 2 : 1;
-                Excel.Range c1, c2;
-
-                c1 = (Excel.Range)ws.Cells[startRow, startCol];
-                c2 = (Excel.Range)ws.Cells[rows + startRow - 1, cols + startCol - 1];
-                Excel.Range dataRange = ws.get_Range(c1, c2);
-                dataRange.Value = arr;
-
-                if (rowNames != null) {
-                    for (int r = 0; r < rows; r++) {
-                        ws.Cells[r + startRow, 1] = rowNames[r];
+                    if (ws == null) {
+                        ws = (Worksheet)wb.Sheets.Add();
                     }
 
-                    c1 = (Excel.Range)ws.Cells[startRow, 1];
-                    c2 = (Excel.Range)ws.Cells[rows + startRow - 1, 1];
-                    Excel.Range rowNamesRange = ws.get_Range(c1, c2);
-                    rowNamesRange.Columns.AutoFit();
+                    PopulateWorksheet(ws, xlData.RowNames, xlData.ColNames, xlData.CellData);
+                    _excel.Visible = true;
                 }
-
-                if (colNames != null) {
-                    c1 = (Excel.Range)ws.Cells[1, startCol];
-                    c2 = (Excel.Range)ws.Cells[1, cols + startCol - 1];
-                    Excel.Range colRange = ws.get_Range(c1, c2);
-                    colRange.Value = colNames;
-                    colRange.HorizontalAlignment = XlHAlign.xlHAlignRight;
-                }
-
-                _excel.Visible = true;
             } finally {
                 _busy = false;
+            }
+        }
+
+        private static async Task<ExcelData> GenerateExcelData(string expression, int rows, int cols) {
+            ExcelData xlData = new ExcelData();
+
+            IGridData<string> data = await GridDataSource.GetGridDataAsync(expression,
+                                     new GridRange(new Range(0, rows), new Range(0, cols)));
+            if (data.RowHeader != null) {
+                xlData.RowNames = await MakeRowNames(data, rows);
+            }
+
+            if (data.RowHeader != null) {
+                xlData.ColNames = await MakeColumnNames(data, cols);
+            }
+
+            xlData.CellData = await MakeExcelRange(data, rows, cols);
+            return xlData;
+        }
+
+        private static void PopulateWorksheet(Worksheet ws, object[] rowNames, object[] colNames, object[,] cellData) {
+            int startRow = colNames != null ? 2 : 1;
+            int startCol = rowNames != null ? 2 : 1;
+            Excel.Range c1, c2;
+
+            int rows = rowNames.Length;
+            int cols = colNames.Length;
+
+            c1 = (Excel.Range)ws.Cells[startRow, startCol];
+            c2 = (Excel.Range)ws.Cells[rows + startRow - 1, cols + startCol - 1];
+            Excel.Range dataRange = ws.get_Range(c1, c2);
+            dataRange.Value = cellData;
+
+            if (rowNames != null) {
+                for (int r = 0; r < rows; r++) {
+                    ws.Cells[r + startRow, 1] = rowNames[r];
+                }
+
+                c1 = (Excel.Range)ws.Cells[startRow, 1];
+                c2 = (Excel.Range)ws.Cells[rows + startRow - 1, 1];
+                Excel.Range rowNamesRange = ws.get_Range(c1, c2);
+                rowNamesRange.Columns.AutoFit();
+            }
+
+            if (colNames != null) {
+                c1 = (Excel.Range)ws.Cells[1, startCol];
+                c2 = (Excel.Range)ws.Cells[1, cols + startCol - 1];
+                Excel.Range colRange = ws.get_Range(c1, c2);
+                colRange.Value = colNames;
+                colRange.HorizontalAlignment = XlHAlign.xlHAlignRight;
             }
         }
 
