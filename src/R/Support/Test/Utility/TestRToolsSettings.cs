26a2f43e7f096268e69d4058e2398f0699b3139a
Pavel Minaev <pminaev@microsoft.com>
2016-02-08 12:59:30 -0800

    Fix SteppingTest.StepOver.

--- src/R/Support/Test/Utility/TestRToolsSettings.cs
+++ src/R/Support/Test/Utility/TestRToolsSettings.cs
@@ -18,11 +18,25 @@ namespace Microsoft.R.Support.Test.Utility {
             get {
                 // Test settings are fixed and are unrelated to what is stored in VS.
                 // Therefore we need to look up R when it is not in the registry.
-                foreach(string dir in Directory.EnumerateDirectories(@"C:\Program Files\R")) {
-                    if(dir.Contains("R-3.2")) {
-                        return dir;
+                string programFiles = Environment.GetEnvironmentVariable("ProgramW6432");
+                if (programFiles == null) {
+                    return string.Empty;
+                }
+
+                var topDir = new DirectoryInfo(Path.Combine(programFiles, "R"));
+                if (!topDir.Exists) {
+                    topDir = new DirectoryInfo(Path.Combine(programFiles, @"Microsoft\MRO-for-RRE\8.0"));
+                    if (!topDir.Exists) {
+                        return string.Empty;
                     }
                 }
+
+                foreach (var dir in topDir.EnumerateDirectories()) {
+                    if (dir.Name.StartsWith("R-3.2.")) {
+                        return dir.FullName;
+                    }
+                }
+
                 return string.Empty;
             }
             set { }
