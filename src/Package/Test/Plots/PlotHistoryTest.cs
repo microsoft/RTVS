2e26a1168db3c121b22c37e67598fb3c1a3441b0
huguesv <huvalo@microsoft.com>
2016-02-11 09:03:40 -0800

    Use await instead of Wait() to unwrap the AggregateExceptions.

--- src/Package/Test/Plots/PlotHistoryTest.cs
+++ src/Package/Test/Plots/PlotHistoryTest.cs
@@ -135,24 +135,24 @@ namespace Microsoft.VisualStudio.R.Package.Test.Plots {
                 foreach (var c in _commands) {
                     using (var interaction = await script.Session.BeginInteractionAsync()) {
                         try {
-                            interaction.RespondAsync(c + Environment.NewLine).Wait(1000);
+                            await interaction.RespondAsync(c + Environment.NewLine);
                             EventsPump.DoEvents(100);
-                        } catch (RException) { } catch(AggregateException) { }
+                        } catch (RException) { }
                     }
                 }
 
                 for (int i = _commands.Length - 1; i >= 0; i--) {
                     try {
-                        history.PlotContentProvider.PreviousPlotAsync().Wait(1000);
+                        await history.PlotContentProvider.PreviousPlotAsync();
                         EventsPump.DoEvents(100);
-                    } catch (RException) { } catch (AggregateException) { }
+                    } catch (RException) { }
                 }
 
                 for (int i = 0; i < _commands.Length; i++) {
                     try {
-                        history.PlotContentProvider.NextPlotAsync().Wait(1000);
+                        await history.PlotContentProvider.NextPlotAsync();
                         EventsPump.DoEvents(500);
-                    } catch (RException) { } catch (AggregateException) { }
+                    } catch (RException) { }
                 }
 
                 EventsPump.DoEvents(1000);
