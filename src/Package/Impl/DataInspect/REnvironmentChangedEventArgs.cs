9d5c3442c22a12d0ce3112f5d413e87842951aa3
JeongPyoHam <jeongham@microsoft.com>
2016-03-21 11:12:54 -0700

    Classes for env

---
+++ src/Package/Impl/DataInspect/REnvironmentChangedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+
+namespace Microsoft.VisualStudio.R.Package.DataInspect {
+    internal class REnvironmentChangedEventArgs : EventArgs {
+        public REnvironmentCollection Environments;
+    }
+}
---
+++ src/Package/Impl/DataInspect/REnvironmentProvider.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using Microsoft.R.Host.Client;
+using Microsoft.VisualStudio.R.Package.DataInspect.Definitions;
+
+namespace Microsoft.VisualStudio.R.Package.DataInspect {
+    internal class REnvironmentProvider : IREnvironmentProvider {
+        private IRSession _rSession;
+
+        public REnvironmentProvider(IRSession session) {
+            _rSession = session;
+        }
+
+        #region IEnvironmentContentProvider
+
+        public event EventHandler<REnvironmentChangedEventArgs> EnvironmentChanged;
+
+        #endregion
+    }
+}
