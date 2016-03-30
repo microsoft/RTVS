31229b97f41a68ff08d0c31022444976de3a084a
MikhailArkhipov <mikhaila@microsoft.com>
2016-03-29 20:22:59 -0700

    Propagate snippet description

--- src/Package/Impl/Repl/Commands/ReplShortcutSetting.cs
+++ src/Package/Impl/Repl/Commands/ReplShortcutSetting.cs
@@ -10,11 +10,11 @@ using Microsoft.VisualStudio.R.Package.Shell;
 namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
     internal static class ReplShortcutSetting {
         private static Dictionary<string, string> _editorShortcutRemaps = new Dictionary<string, string>() {
-            { "Edit.LineOpenAbove", "Text Editor::ctrl+k,ctrl+a" }, // Make Ctrl+Enter work in R Editor
-            { "Edit.LineCut", "Text Editor::ctrl+k,ctrl+x" }        // Make Ctrl+L work in editor 
+            { "Edit.LineOpenAbove", "Text Editor::ctrl+k,ctrl+o" }, // Make Ctrl+Enter work in R Editor
+            { "Edit.LineCut", "Text Editor::ctrl+k,ctrl+t" }        // Make Ctrl+L work in editor 
         };
         private static Dictionary<string, string> _globalShortcutRemaps = new Dictionary<string, string>() {
-            { "Edit.LineCut", "global::ctrl+k,ctrl+x" }             // Make Ctrl+L work globally 
+            { "Edit.LineCut", "global::ctrl+k,ctrl+t" }             // Make Ctrl+L work globally 
         };
 
         public static void Initialize() {
