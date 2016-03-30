7dd208dc7b789bbd8312594d765b10ad02f26325
Mikhail Arkhipov <mikhaila@outlook.com>
2016-03-30 14:58:57 -0700

    Avoid importing RS keyboard

--- src/Package/Impl/Options/R/Tools/ImportRSettingsCommand.cs
+++ src/Package/Impl/Options/R/Tools/ImportRSettingsCommand.cs
@@ -27,7 +27,7 @@ namespace Microsoft.VisualStudio.R.Package.Options.R.Tools {
 
                 string asmDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetAssemblyPath());
                 string settingsFilePath1 = Path.Combine(asmDirectory, @"Profiles\", "R.vssettings");
-                string settingsFilePath2 = Path.Combine(asmDirectory, @"Profiles\", "RStudioKeyboard.vssettings");
+                //string settingsFilePath2 = Path.Combine(asmDirectory, @"Profiles\", "RStudioKeyboard.vssettings");
                 if (!File.Exists(settingsFilePath1)) {
                     string ideFolder = asmDirectory.Substring(0, asmDirectory.IndexOf(@"\Extensions", StringComparison.OrdinalIgnoreCase));
                     settingsFilePath1 = Path.Combine(ideFolder, @"Profiles\", "R.vssettings");
@@ -36,10 +36,10 @@ namespace Microsoft.VisualStudio.R.Package.Options.R.Tools {
                 object arguments = string.Format(CultureInfo.InvariantCulture, "-import:\"{0}\"", settingsFilePath1);
                 shell.PostExecCommand(ref group, (uint)VSConstants.VSStd2KCmdID.ManageUserSettings, 0, ref arguments);
 
-                if (MessageButtons.Yes == VsAppShell.Current.ShowMessage(Resources.Warning_RStudioKeyboardShortcuts, MessageButtons.YesNo)) {
-                    arguments = string.Format(CultureInfo.InvariantCulture, "-import:\"{0}\"", settingsFilePath2);
-                    shell.PostExecCommand(ref group, (uint)VSConstants.VSStd2KCmdID.ManageUserSettings, 0, ref arguments);
-                }
+                //if (MessageButtons.Yes == VsAppShell.Current.ShowMessage(Resources.Warning_RStudioKeyboardShortcuts, MessageButtons.YesNo)) {
+                //    arguments = string.Format(CultureInfo.InvariantCulture, "-import:\"{0}\"", settingsFilePath2);
+                //    shell.PostExecCommand(ref group, (uint)VSConstants.VSStd2KCmdID.ManageUserSettings, 0, ref arguments);
+                //}
             }
         }
     }
