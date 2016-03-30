c8fa07164a98a44707ebb284f14a3a4639327987
MikhailArkhipov <mikhaila@microsoft.com>
2016-03-29 20:04:02 -0700

    Ctrl+L global

--- src/Package/Impl/Repl/Commands/ReplShortcutSetting.cs
+++ src/Package/Impl/Repl/Commands/ReplShortcutSetting.cs
@@ -6,13 +6,15 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using EnvDTE;
 using Microsoft.VisualStudio.R.Package.Shell;
-using static System.FormattableString;
 
 namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
     internal static class ReplShortcutSetting {
-        private static Dictionary<string, string> _shortcutRemaps = new Dictionary<string, string>() {
-            { "Edit.LineOpenAbove", "ctrl+k,ctrl+a" },
-            { "Edit.LineCut", "ctrl+k,ctrl+x" }
+        private static Dictionary<string, string> _editorShortcutRemaps = new Dictionary<string, string>() {
+            { "Edit.LineOpenAbove", "Text Editor::ctrl+k,ctrl+a" }, // Make Ctrl+Enter work in R Editor
+            { "Edit.LineCut", "Text Editor::ctrl+k,ctrl+x" }        // Make Ctrl+L work in editor 
+        };
+        private static Dictionary<string, string> _globalShortcutRemaps = new Dictionary<string, string>() {
+            { "Edit.LineCut", "global::ctrl+k,ctrl+x" }             // Make Ctrl+L work globally 
         };
 
         public static void Initialize() {
@@ -22,13 +24,18 @@ namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
         private static void SetBinding() {
             DTE dte = VsAppShell.Current.GetGlobalService<DTE>();
             try {
-                foreach (var kvp in _shortcutRemaps) {
-                    var command = dte.Commands.Item(kvp.Key);
-                    if (command != null) {
-                        command.Bindings = Invariant($"Text Editor::{kvp.Value}");
-                    }
-                }
+                dte.ApplyShortcuts(_editorShortcutRemaps);
+                dte.ApplyShortcuts(_globalShortcutRemaps);
             } catch (ArgumentException) { } catch(COMException) { }
+        }
+
+        private static void ApplyShortcuts(this DTE dte, IReadOnlyDictionary<string, string> shortcuts) {
+            foreach (var kvp in shortcuts) {
+                var command = dte.Commands.Item(kvp.Key);
+                if (command != null) {
+                    command.Bindings = kvp.Value;
+                }
+            }
         }
     }
 }
