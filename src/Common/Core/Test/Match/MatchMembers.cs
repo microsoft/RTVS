bfc0dda13efb56aea6b02af96573da9a228f75d6
Pavel Minaev <int19h@gmail.com>
2016-04-04 13:14:40 -0700

    Fix and simplify MatchMembers.

--- src/Common/Core/Test/Match/MatchMembers.cs
+++ src/Common/Core/Test/Match/MatchMembers.cs
@@ -10,63 +10,47 @@ using FluentAssertions.Formatting;
 namespace Microsoft.Common.Core.Test.Match {
     public class MatchMembers<T> : IEquatable<T> {
         private readonly MatchMembers<T> _next;
-        private readonly MemberExpression _member;
-        private readonly Func<T, object> _selector;
-        private readonly object _value;
+        private readonly string _memberName;
+        private readonly object _expected;
+        private readonly Func<T, bool> _equals;
 
         static MatchMembers() {
             FluentAssertions.Formatting.Formatter.AddFormatter(new Formatter());
         }
 
         public MatchMembers() {
+            _equals = x => true;
         }
 
-        private MatchMembers(MatchMembers<T> next, LambdaExpression memberSelector, object value) {
-            _member = memberSelector?.Body as MemberExpression;
-            if (_member == null) {
+        private MatchMembers(MatchMembers<T> next, LambdaExpression memberSelector, object expected, Func<T, bool> equals) {
+            var member = memberSelector?.Body as MemberExpression;
+            if (member == null) {
                 throw new ArgumentException("Member selector must be of the form `x => x.Member`.", "memberSelector");
             }
 
-            _selector = Expression.Lambda<Func<T, object>>(Expression.TypeAs(_member, typeof(object)), memberSelector.Parameters[0]).Compile();
-            _value = value;
+            _memberName = member.Member.Name;
             _next = next;
+            _expected = expected;
+            _equals = equals;
         }
 
-        public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember>> memberSelector, IEquatable<TMember> value) =>
-            new MatchMembers<T>(this, memberSelector, value);
+        public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember>> memberSelector, IEquatable<TMember> expected) =>
+            new MatchMembers<T>(this, memberSelector, expected, x => Equals(expected, memberSelector.Compile()(x)));
 
         public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember>> memberSelector, IComparable<TMember> from, IComparable<TMember> to)
             where TMember : IComparable<TMember> =>
-            new MatchMembers<T>(this, memberSelector, new MatchRange<TMember>(from, to));
+            Matching(memberSelector, new MatchRange<TMember>(from, to));
 
-        public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember?>> memberSelector, IEquatable<TMember> value)
+        public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember?>> memberSelector, IEquatable<TMember> expected)
             where TMember : struct =>
-            new MatchMembers<T>(this, memberSelector, value);
+            new MatchMembers<T>(this, memberSelector, expected, x => Equals(expected, memberSelector.Compile()(x)));
 
         public MatchMembers<T> Matching<TMember>(Expression<Func<T, TMember?>> memberSelector, IComparable<TMember> from, IComparable<TMember> to)
             where TMember : struct, IComparable<TMember> =>
-            new MatchMembers<T>(this, memberSelector, new MatchRange<TMember>(from, to));
+            Matching(memberSelector, new MatchRange<TMember>(from, to));
 
-        public bool Equals(T other) {
-            if (other == null) {
-                return false;
-            } else if (_member == null) {
-                return true;
-            }
-
-            object actualValue = _selector(other);
-            if (_value == null) {
-                if (actualValue != null) {
-                    return false;
-                }
-            } else {
-                if (!_value.Equals(actualValue)) {
-                    return false;
-                }
-            }
-
-            return _next?.Equals(other) ?? true;
-        }
+        public bool Equals(T other) =>
+            other != null && _equals(other) && _next?.Equals(other) != false;
 
         public override int GetHashCode() {
             throw new NotSupportedException(nameof(MatchMembers<T>) + " does not support " + nameof(GetHashCode));
@@ -82,10 +66,12 @@ namespace Microsoft.Common.Core.Test.Match {
                 value is MatchMembers<T>;
 
             public string ToString(object value, bool useLineBreaks, IList<object> processedObjects = null, int nestedPropertyLevel = 0) {
-                if (processedObjects.Contains(value)) {
-                    return string.Format("{{Cyclic reference to type {0} detected}}", value.GetType());
-                } else {
-                    processedObjects.Add(value);
+                if (processedObjects != null) {
+                    if (processedObjects.Contains(value)) {
+                        return string.Format("{{Cyclic reference to type {0} detected}}", value.GetType());
+                    } else {
+                        processedObjects.Add(value);
+                    }
                 }
 
                 var sb = new StringBuilder();
@@ -102,11 +88,11 @@ namespace Microsoft.Common.Core.Test.Match {
                 sb.AppendLine(Indent(nestedPropertyLevel) + "{");
 
                 for (var match = (MatchMembers<T>)value; match != null; match = match._next) {
-                    if (match._member != null) {
+                    if (match._memberName != null) {
                         sb.Append(Indent(nestedPropertyLevel + 1));
-                        sb.Append(match._member.Member.Name);
+                        sb.Append(match._memberName);
                         sb.Append(" = ");
-                        sb.AppendLine(FluentAssertions.Formatting.Formatter.ToString(match._value, false, processedObjects, nestedPropertyLevel + 1));
+                        sb.AppendLine(FluentAssertions.Formatting.Formatter.ToString(match._expected, false, processedObjects, nestedPropertyLevel + 1));
                     }
                 }
 
