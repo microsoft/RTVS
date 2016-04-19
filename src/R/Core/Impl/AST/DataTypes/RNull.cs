13fce8f2e6cf70fb48b9431b4c7db0a1aaac6c0c
Mikhail Arkhipov <mikhaila@outlook.com>
2016-04-19 13:19:18 -0700

    FxCop

--- src/R/Core/Impl/AST/DataTypes/RNull.cs
+++ src/R/Core/Impl/AST/DataTypes/RNull.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.R.Core.AST.DataTypes.Definitions;
 
 namespace Microsoft.R.Core.AST.DataTypes {
     public sealed class RNull : RObject, IRVector {
-        public static readonly RNull Null = new RNull();
+        private static readonly RNull _null = new RNull();
+        public static RNull Null => _null;
 
         public int Length {
             get { return 0; }
