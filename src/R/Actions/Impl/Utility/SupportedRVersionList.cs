2e91d944554e5276891e26a6b4b3031ce14c5ca9
Mikhail Arkhipov <mikhaila@outlook.com>
2016-05-12 14:54:48 -0700

    #1758 Help On... context item includes the entire selection

--- src/R/Actions/Impl/Utility/SupportedRVersionList.cs
+++ src/R/Actions/Impl/Utility/SupportedRVersionList.cs
@@ -10,7 +10,7 @@ namespace Microsoft.R.Actions.Utility {
         public const int MinMajorVersion = 3;
         public const int MinMinorVersion = 2;
         public const int MaxMajorVersion = 3;
-        public const int MaxMinorVersion = 9;
+        public const int MaxMinorVersion = 3;
 
         public static readonly Version MinVersion = new Version(MinMajorVersion, MinMinorVersion);
         public static readonly Version MaxVersion = new Version(MaxMajorVersion, MaxMinorVersion);
