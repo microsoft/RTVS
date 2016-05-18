31b31c12e216433569147e7291b2df68a944ef37
Hugues Valois <huvalo@microsoft.com>
2016-05-18 11:18:56 -0700

    Add SilenceException when setting the options width.

--- src/R/Components/Impl/InteractiveWorkflow/Implementation/RInteractiveEvaluator.cs
+++ src/R/Components/Impl/InteractiveWorkflow/Implementation/RInteractiveEvaluator.cs
@@ -226,7 +226,7 @@ namespace Microsoft.R.Components.InteractiveWorkflow.Implementation {
             // From R docs:  Valid values are 10...10000 with default normally 80.
             _terminalWidth = Math.Max(10, Math.Min(10000, width));
 
-            Session.OptionsSetWidthAsync(_terminalWidth).DoNotWait();
+            Session.OptionsSetWidthAsync(_terminalWidth).SilenceException<RException>().SilenceException<MessageTransportException>().DoNotWait();
         }
     }
 }
