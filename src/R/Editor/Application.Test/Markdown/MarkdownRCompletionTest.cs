90bc8e137e37d12e81df12ecb809f49316481d52
MikhailArkhipov <mikhaila@microsoft.com>
2016-05-23 07:10:06 -0700

    signature test in rmd

--- src/R/Editor/Application.Test/Markdown/MarkdownRCompletionTest.cs
+++ src/R/Editor/Application.Test/Markdown/MarkdownRCompletionTest.cs
@@ -3,9 +3,16 @@
 
 using System.Diagnostics.CodeAnalysis;
 using FluentAssertions;
+using Microsoft.Languages.Editor.Shell;
 using Microsoft.Markdown.Editor.ContentTypes;
 using Microsoft.R.Editor.Application.Test.TestShell;
+using Microsoft.R.Host.Client;
+using Microsoft.R.Host.Client.Signatures;
+using Microsoft.R.Host.Client.Test.Script;
+using Microsoft.R.Support.Help.Functions;
+using Microsoft.R.Support.Test.Utility;
 using Microsoft.UnitTests.Core.XUnit;
+using Microsoft.VisualStudio.Language.Intellisense;
 using Xunit;
 
 namespace Microsoft.R.Editor.Application.Test.Markdown {
@@ -15,7 +22,7 @@ namespace Microsoft.R.Editor.Application.Test.Markdown {
         [Test]
         [Category.Interactive]
         public void TypeRBlock() {
-            using (var script = new TestScript("", MdContentTypeDefinition.ContentType)) {
+            using (var script = new TestScript(MdContentTypeDefinition.ContentType)) {
                 script.Type("```{r}{ENTER}{ENTER}```");
                 script.MoveUp();
                 script.Type("x");
@@ -36,6 +43,43 @@ x <- function() {
 ```";
                 string actual = script.EditorText;
                 actual.Should().Be(expected);
+            }
+        }
+
+        [Test]
+        [Category.Interactive]
+        public void RSignature() {
+            using (var script = new TestScript("```{r}\r\n\r\n```", MdContentTypeDefinition.ContentType)) {
+                FunctionRdDataProvider.HostStartTimeout = 10000;
+                using (new RHostScript(EditorShell.Current.ExportProvider.GetExportedValue<IRSessionProvider>())) {
+                    FunctionIndex.Initialize();
+                    FunctionIndex.BuildIndexAsync().Wait();
+                    FunctionIndexUtility.GetFunctionInfoAsync("lm").Wait(3000);
+
+                    script.MoveDown();
+                    script.Type("x <- lm(");
+                    script.DoIdle(2000);
+
+                    ISignatureHelpSession session = script.GetSignatureSession();
+                    session.Should().NotBeNull();
+                    IParameter parameter = session.SelectedSignature.CurrentParameter;
+                    parameter.Should().NotBeNull();
+                    parameter.Name.Should().Be("formula");
+
+                    script.Type("sub");
+                    script.DoIdle(500);
+                    script.Type("{TAB}");
+                    script.DoIdle(1000);
+
+                    parameter = session.SelectedSignature.CurrentParameter;
+                    parameter.Name.Should().Be("subset");
+
+                    string actual = script.EditorText;
+                    actual.Should().Be("```{r}\r\nx <- lm(subset = )\r\n```");
+
+                    session = script.GetSignatureSession();
+                    parameter = session.SelectedSignature.CurrentParameter;
+                }
             }
         }
     }
