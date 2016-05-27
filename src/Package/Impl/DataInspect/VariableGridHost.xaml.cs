0e49ba4e7e7e04e260b6a6f85fce1f7545e790c7
Mikhail Arkhipov <mikhaila@outlook.com>
2016-05-27 13:43:36 -0700

    Better check

--- src/Package/Impl/DataInspect/VariableGridHost.xaml.cs
+++ src/Package/Impl/DataInspect/VariableGridHost.xaml.cs
@@ -54,15 +54,11 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect {
         internal void SetEvaluation(VariableViewModel wrapper) {
             VsAppShell.Current.AssertIsOnMainThread();
 
-            bool evalDimensionsCorrect = _evaluation != null && _evaluation.Dimensions != null && _evaluation.Dimensions.Count == 2;
-            bool wrapperDimensionsCorrect = wrapper.Dimensions != null && wrapper.Dimensions.Count == 2;
-            bool gridSizeChanged = evalDimensionsCorrect && evalDimensionsCorrect && 
-                                 (wrapper.Dimensions[0] != _evaluation.Dimensions[0] || wrapper.Dimensions[1] != _evaluation.Dimensions[1]);
-
-            if (wrapper.TypeName == "NULL" && wrapper.Value == "NULL" || !evalDimensionsCorrect || !wrapperDimensionsCorrect) {
+            if (wrapper.TypeName == "NULL" && wrapper.Value == "NULL") {
                 // the variable should have been removed
                 SetError(string.Format(CultureInfo.InvariantCulture, Package.Resources.VariableGrid_Missing, wrapper.Expression));
-            } else if (_evaluation == null || gridSizeChanged) {
+            } else if (_evaluation == null
+                || (wrapper.Dimensions.Count == 2 && (wrapper.Dimensions[0] != _evaluation.Dimensions[0] || wrapper.Dimensions[1] != _evaluation.Dimensions[1]))) {
                 // matrix size changed. Reset the evaluation
                 ClearError();
                 VariableGrid.Initialize(new GridDataProvider(wrapper));
