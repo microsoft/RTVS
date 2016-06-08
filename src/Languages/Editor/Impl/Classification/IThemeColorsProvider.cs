3497625cc78e46983358f50cfa45f300983fbfdf
Mikhail Arkhipov <mikhaila@outlook.com>
2016-06-07 17:04:43 -0700

    Theme color provider

---
+++ src/Languages/Editor/Impl/Classification/IThemeColorsProvider.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Windows.Media;
+
+namespace Microsoft.Languages.Editor.Classification {
+    public interface IThemeColorsProvider {
+        event EventHandler ThemeChanged;
+
+        Brush CodeBackgroundColor { get; }
+    }
+}
