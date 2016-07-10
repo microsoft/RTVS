36d58b05e3fe803a473b859a0d73cc7208967f5e
MikhailArkhipov <mikhaila@microsoft.com>
2016-07-10 11:09:11 -0700

    Drag-drop

---
+++ src/Package/Impl/ProjectSystem/WorkspaceServices.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.Languages.Editor.Workspace;
+
+namespace Microsoft.VisualStudio.R.Package.ProjectSystem {
+    [Export(typeof(IWorkspaceServices))]
+    internal sealed class WorkspaceServices : IWorkspaceServices {
+        [Import]
+        private IProjectSystemServices ProjectSystemServices { get; set; }
+
+        public string ActiveProjectPath {
+            get {
+                var projectFile = ProjectSystemServices.GetActiveProject()?.FullName;
+                return !string.IsNullOrEmpty(projectFile) ? Path.GetDirectoryName(projectFile) : string.Empty;
+            }
+        }
+    }
+}
---
+++ src/R/Editor/Impl/DragDrop/DataObject.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Common.Core;
+using Microsoft.Languages.Editor.DragDrop;
+using Microsoft.R.Host.Client.Extensions;
+using static System.FormattableString;
+
+namespace Microsoft.R.Editor.DragDrop {
+    public static class DataObject {
+        public static string GetPlainText(this IDataObject dataObject, string projectFolder) {
+            var flags = dataObject.GetFlags();
+            if ((flags & DataObjectFlags.ProjectItems) != 0) {
+                return dataObject.TextFromProjectItems(projectFolder);
+            }
+            return string.Empty;
+        }
+
+        private static string TextFromProjectItems(this IDataObject dataObject, string projectFolder) {
+            var items = dataObject.GetProjectItems();
+            var fileNames = items.Select(n => n.FileName);
+
+            var sb = new StringBuilder();
+            foreach(var item in items) {
+                var relative = item.FileName.MakeRRelativePath(projectFolder);
+                if (Path.GetExtension(item.FileName).EqualsIgnoreCase(".R")) {
+                    // Convert to R project relative
+                     sb.AppendLine(Invariant($"source('{relative}')"));
+                } else {
+                    sb.AppendLine(Invariant($"'{relative}'"));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
