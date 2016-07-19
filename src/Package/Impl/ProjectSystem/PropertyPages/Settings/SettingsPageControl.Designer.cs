601c98e09c92724f575313647d99fccd4511a822
Mikhail Arkhipov <mikhaila@outlook.com>
2016-07-19 13:21:26 -0700

    #2092 Settings page tab order Add SQL query template to setup

--- src/Package/Impl/ProjectSystem/PropertyPages/Settings/SettingsPageControl.Designer.cs
+++ src/Package/Impl/ProjectSystem/PropertyPages/Settings/SettingsPageControl.Designer.cs
@@ -37,10 +37,10 @@
             // 
             // propertyGrid
             // 
-            this.propertyGrid.Location = new System.Drawing.Point(0, 56);
-            this.propertyGrid.Margin = new System.Windows.Forms.Padding(37, 37, 37, 37);
+            this.propertyGrid.Location = new System.Drawing.Point(0, 30);
+            this.propertyGrid.Margin = new System.Windows.Forms.Padding(20, 20, 20, 20);
             this.propertyGrid.Name = "propertyGrid";
-            this.propertyGrid.Size = new System.Drawing.Size(803, 391);
+            this.propertyGrid.Size = new System.Drawing.Size(438, 212);
             this.propertyGrid.TabIndex = 1;
             this.propertyGrid.ToolbarVisible = false;
             // 
@@ -49,25 +49,22 @@
             this.filesList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.filesList.FormattingEnabled = true;
             this.filesList.Location = new System.Drawing.Point(0, 0);
-            this.filesList.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.filesList.Name = "filesList";
-            this.filesList.Size = new System.Drawing.Size(455, 32);
+            this.filesList.Size = new System.Drawing.Size(250, 21);
             this.filesList.TabIndex = 0;
             // 
             // variableName
             // 
-            this.variableName.Location = new System.Drawing.Point(1, 488);
-            this.variableName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.variableName.Location = new System.Drawing.Point(1, 264);
             this.variableName.Name = "variableName";
-            this.variableName.Size = new System.Drawing.Size(132, 29);
+            this.variableName.Size = new System.Drawing.Size(74, 20);
             this.variableName.TabIndex = 3;
             // 
             // addSettingButton
             // 
-            this.addSettingButton.Location = new System.Drawing.Point(665, 481);
-            this.addSettingButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.addSettingButton.Location = new System.Drawing.Point(363, 261);
             this.addSettingButton.Name = "addSettingButton";
-            this.addSettingButton.Size = new System.Drawing.Size(138, 42);
+            this.addSettingButton.Size = new System.Drawing.Size(75, 23);
             this.addSettingButton.TabIndex = 8;
             this.addSettingButton.Text = "&Add";
             this.addSettingButton.UseVisualStyleBackColor = true;
@@ -75,65 +72,60 @@
             // variableNameLabel
             // 
             this.variableNameLabel.AutoSize = true;
-            this.variableNameLabel.Location = new System.Drawing.Point(-4, 457);
-            this.variableNameLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.variableNameLabel.Location = new System.Drawing.Point(-2, 248);
             this.variableNameLabel.Name = "variableNameLabel";
-            this.variableNameLabel.Size = new System.Drawing.Size(70, 25);
+            this.variableNameLabel.Size = new System.Drawing.Size(38, 13);
             this.variableNameLabel.TabIndex = 2;
             this.variableNameLabel.Text = "Name:";
             // 
             // explanationText
             // 
             this.explanationText.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.explanationText.Location = new System.Drawing.Point(825, 56);
-            this.explanationText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.explanationText.Location = new System.Drawing.Point(450, 30);
             this.explanationText.Multiline = true;
             this.explanationText.Name = "explanationText";
             this.explanationText.ReadOnly = true;
-            this.explanationText.Size = new System.Drawing.Size(508, 391);
+            this.explanationText.Size = new System.Drawing.Size(277, 212);
             this.explanationText.TabIndex = 6;
+            this.explanationText.TabStop = false;
             // 
             // variableValueLabel
             // 
             this.variableValueLabel.AutoSize = true;
-            this.variableValueLabel.Location = new System.Drawing.Point(159, 459);
-            this.variableValueLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.variableValueLabel.Location = new System.Drawing.Point(87, 249);
             this.variableValueLabel.Name = "variableValueLabel";
-            this.variableValueLabel.Size = new System.Drawing.Size(69, 25);
+            this.variableValueLabel.Size = new System.Drawing.Size(37, 13);
             this.variableValueLabel.TabIndex = 4;
             this.variableValueLabel.Text = "Value:";
             // 
             // variableValue
             // 
-            this.variableValue.Location = new System.Drawing.Point(164, 488);
-            this.variableValue.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.variableValue.Location = new System.Drawing.Point(89, 264);
             this.variableValue.Name = "variableValue";
-            this.variableValue.Size = new System.Drawing.Size(286, 29);
+            this.variableValue.Size = new System.Drawing.Size(158, 20);
             this.variableValue.TabIndex = 5;
             // 
             // variableTypeList
             // 
             this.variableTypeList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.variableTypeList.FormattingEnabled = true;
-            this.variableTypeList.Location = new System.Drawing.Point(483, 486);
-            this.variableTypeList.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.variableTypeList.Location = new System.Drawing.Point(263, 263);
             this.variableTypeList.Name = "variableTypeList";
-            this.variableTypeList.Size = new System.Drawing.Size(142, 32);
+            this.variableTypeList.Size = new System.Drawing.Size(79, 21);
             this.variableTypeList.TabIndex = 7;
             // 
             // variableTypeLabel
             // 
             this.variableTypeLabel.AutoSize = true;
-            this.variableTypeLabel.Location = new System.Drawing.Point(476, 455);
-            this.variableTypeLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.variableTypeLabel.Location = new System.Drawing.Point(260, 246);
             this.variableTypeLabel.Name = "variableTypeLabel";
-            this.variableTypeLabel.Size = new System.Drawing.Size(63, 25);
+            this.variableTypeLabel.Size = new System.Drawing.Size(34, 13);
             this.variableTypeLabel.TabIndex = 6;
             this.variableTypeLabel.Text = "Type:";
             // 
             // SettingsPageControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
@@ -147,10 +139,9 @@
             this.Controls.Add(this.variableName);
             this.Controls.Add(this.filesList);
             this.Controls.Add(this.propertyGrid);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.MinimumSize = new System.Drawing.Size(367, 369);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
             this.Name = "SettingsPageControl";
-            this.Size = new System.Drawing.Size(1339, 529);
+            this.Size = new System.Drawing.Size(730, 287);
             this.ResumeLayout(false);
             this.PerformLayout();
 
