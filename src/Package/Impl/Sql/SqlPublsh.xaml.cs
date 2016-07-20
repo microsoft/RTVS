6dac0bb522ce5687c453c9513d3d5709b7c369a8
Mikhail Arkhipov <mikhaila@outlook.com>
2016-07-20 16:12:24 -0700

    Sql publish dialog

---
+++ src/Package/Impl/Sql/SqlPublsh.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Microsoft.VisualStudio.R.Package.Sql {
+    /// <summary>
+    /// Interaction logic for SqlPublsh.xaml
+    /// </summary>
+    public partial class SqlPublsh : UserControl {
+        public SqlPublsh() {
+            InitializeComponent();
+        }
+    }
+}
