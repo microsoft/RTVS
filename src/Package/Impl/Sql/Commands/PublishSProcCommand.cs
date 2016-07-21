8d46ea0a75419768ad72d6d99d8e17f4bd069db1
MikhailArkhipov <mikhaila@microsoft.com>
2016-07-21 12:24:47 -0700

    Publish sproc command

---
+++ src/Package/Impl/Sql/Commands/PublishSProcCommand.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.R.Components.InteractiveWorkflow;
+using Microsoft.VisualStudio.R.Package.Commands;
+using Microsoft.VisualStudio.R.Package.Shell;
+
+namespace Microsoft.VisualStudio.R.Package.Sql {
+    internal sealed class PublishSProcCommand : SessionCommand {
+        private readonly IApplicationShell _appShell;
+
+        public PublishSProcCommand(IApplicationShell appShell, IRInteractiveWorkflow workflow) : 
+            base(RPackageCommandId.icmdPublishSProc, workflow) {
+            _appShell = appShell;
+        }
+
+        protected override void Handle() {
+            var dlg = new SqlPublshDialog();
+            dlg.ShowModal();
+        }
+    }
+}
---
+++ src/Package/Impl/Sql/SqlPublshDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace Microsoft.VisualStudio.R.Package.Sql {
+    /// <summary>
+    /// Interaction logic for SqlPublsh.xaml
+    /// </summary>
+    public partial class SqlPublshDialog : DialogWindow {
+        public SqlPublshDialog() {
+            InitializeComponent();
+        }
+    }
+}
