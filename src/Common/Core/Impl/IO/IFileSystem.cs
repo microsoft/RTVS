ebda9bb268965b4a2c80a1697199e1f1f5987595
Karthik Nadig <kanadig@microsoft.com>
2016-08-01 16:25:16 -0700

    #2121 CsvAppFileIO changed to use IApplicationShell, IFileSystem, and IProcessServices

--- src/Common/Core/Impl/IO/IFileSystem.cs
+++ src/Common/Core/Impl/IO/IFileSystem.cs
@@ -22,6 +22,7 @@ namespace Microsoft.Common.Core.IO {
 
         IFileVersionInfo GetVersionInfo(string path);
         void DeleteFile(string path);
+        void DeleteDirectory(string path, bool recursive);
         string[] GetFileSystemEntries(string path, string searchPattern, SearchOption options);
     }
 }
--- src/Package/Impl/DataInspect/VariableViewModel.cs
+++ src/Package/Impl/DataInspect/VariableViewModel.cs
@@ -8,6 +8,8 @@ using System.Globalization;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Common.Core;
+using Microsoft.Common.Core.IO;
+using Microsoft.Common.Core.OS;
 using Microsoft.R.Components.InteractiveWorkflow;
 using Microsoft.R.DataInspection;
 using Microsoft.R.Editor.Data;
@@ -16,6 +18,7 @@ using Microsoft.R.Host.Client.Host;
 using Microsoft.R.Support.Settings;
 using Microsoft.VisualStudio.PlatformUI;
 using Microsoft.VisualStudio.R.Package.DataInspect.Office;
+using Microsoft.VisualStudio.R.Package.ProjectSystem;
 using Microsoft.VisualStudio.R.Package.Shell;
 using static System.FormattableString;
 using static Microsoft.R.DataInspection.REvaluationResultProperties;
@@ -152,7 +155,7 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect {
         public string OpenInCsvAppCommandTooltip { get; private set; }
 
         private void OpenInCsvApp(object parameter) {
-            CsvAppFileIO.OpenDataCsvApp(DebugEvaluation).DoNotWait();
+            CsvAppFileIO.OpenDataCsvApp(DebugEvaluation, VsAppShell.Current,  new FileSystem(), ProcessServices.Current).DoNotWait();
         }
 
         /// <summary>
