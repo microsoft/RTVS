2da468a66af4f3faf4f495a27abdc1ffac4fea62
Mikhail Arkhipov <mikhaila@outlook.com>
2016-08-02 15:52:06 -0700

    More Dev15 build fixes

--- src/Package/Impl/ProjectSystem/ProjectTreePropertiesProvider.cs
+++ src/Package/Impl/ProjectSystem/ProjectTreePropertiesProvider.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.Composition;
 using System.IO;
 using Microsoft.VisualStudio.Imaging;
 using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.R.Package.Sql;
 
 namespace Microsoft.VisualStudio.R.Package.ProjectSystem {
     [Export(typeof(IProjectTreePropertiesProvider))]
