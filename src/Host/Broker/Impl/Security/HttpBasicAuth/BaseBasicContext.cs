ad78f445570df81f66433d024c94800311ad80bd
Pavel Minaev <int19h@gmail.com>
2016-08-23 17:17:57 -0700

    Remove references to Odachi.AspNetCore.Authentication.Basic, and move the corresponding code directly into the broker assembly, to avoid strong name / no strong name mix-up issues.

---
+++ src/Host/Broker/Impl/Security/HttpBasicAuth/BaseBasicContext.cs
@@ -0,0 +1,25 @@
+﻿// From https://github.com/Kukkimonsuta/Odachi/tree/master/src/Odachi.AspNetCore.Authentication.Basic
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odachi.AspNetCore.Authentication.Basic
+{
+	public class BaseBasicContext : BaseControlContext
+	{
+		public BaseBasicContext(HttpContext context, BasicOptions options)
+			: base(context)
+		{
+			if (options == null)
+				throw new ArgumentNullException(nameof(options));
+
+			Options = options;
+		}
+
+		public BasicOptions Options { get; }
+	}
+}
