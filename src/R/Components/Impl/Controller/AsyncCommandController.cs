41bf2b4c733962bf85e034f2dbfe6782d82f645c
Hugues Valois <hugues_valois@hotmail.com>
2016-08-26 14:10:52 -0700

    Flatten command map in AsyncCommandController.

--- src/R/Components/Impl/Controller/AsyncCommandController.cs
+++ src/R/Components/Impl/Controller/AsyncCommandController.cs
@@ -7,10 +7,7 @@ using Microsoft.Common.Core;
 
 namespace Microsoft.R.Components.Controller {
     public class AsyncCommandController : ICommandTarget {
-        public Dictionary<Guid, Dictionary<int, IAsyncCommand>> CommandMap { get; } = new Dictionary<Guid, Dictionary<int, IAsyncCommand>>();
-
-        public AsyncCommandController() {
-        }
+        private Dictionary<Key, IAsyncCommand> CommandMap { get; } = new Dictionary<Key, IAsyncCommand>();
 
         public CommandResult Invoke(Guid group, int id, object inputArg, ref object outputArg) {
             CommandResult result = CommandResult.NotSupported;
@@ -30,9 +27,9 @@ namespace Microsoft.R.Components.Controller {
 
         public CommandStatus Status(Guid group, int id) {
             IAsyncCommand cmd = Find(group, id);
-
-            if (cmd != null)
+            if (cmd != null) {
                 return cmd.Status;
+            }
 
             return CommandStatus.NotSupported;
         }
@@ -42,28 +39,36 @@ namespace Microsoft.R.Components.Controller {
         /// </summary>
         /// <param name="command">Command object</param>
         public void AddCommand(Guid group, int id, IAsyncCommand command) {
-            Dictionary<int, IAsyncCommand> idToCommandMap = null;
-
-            if (!CommandMap.TryGetValue(group, out idToCommandMap)) {
-                idToCommandMap = new Dictionary<int, IAsyncCommand>();
-                CommandMap.Add(group, idToCommandMap);
-            }
-
-            if (!idToCommandMap.ContainsKey(id)) {
-                idToCommandMap.Add(id, command);
+            var key = new Key(group, id);
+            if (!CommandMap.ContainsKey(key)) {
+                CommandMap.Add(key, command);
             }
         }
 
         public IAsyncCommand Find(Guid group, int id) {
-            Dictionary<int, IAsyncCommand> idToCommandMap = null;
-
-            if (!CommandMap.TryGetValue(group, out idToCommandMap))
-                return null;
-
             IAsyncCommand cmd = null;
-            idToCommandMap.TryGetValue(id, out cmd);
-
+            CommandMap.TryGetValue(new Key(group, id), out cmd);
             return cmd;
+        }
+
+        private struct Key : IEquatable<Key> {
+            public Guid Group { get; }
+            public int Id { get; }
+
+            public Key(Guid group, int id) {
+                Group = group;
+                Id = id;
+            }
+
+            public override bool Equals(object obj) => obj is Key && Equals((Key)obj);
+
+            public bool Equals(Key other) => Group.Equals(other.Group) && Id == other.Id;
+
+            public override int GetHashCode() {
+                unchecked {
+                    return (Group.GetHashCode() * 397) ^ Id;
+                }
+            }
         }
     }
 }
