735d252bdfa5b9338de229e30aae7f02538f801d
Karthik Nadig <kanadig@microsoft.com>
2016-08-29 14:34:57 -0700

    Addressing comments Iteration-1

--- src/Package/Impl/ProjectSystem/Commands/CopyRemoteItemPathCommand.cs
+++ src/Package/Impl/ProjectSystem/Commands/CopyRemoteItemPathCommand.cs
@@ -4,18 +4,14 @@
 using System;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Microsoft.Common.Core;
 using Microsoft.Common.Core.Shell;
 using Microsoft.R.Components.Extensions;
 using Microsoft.R.Components.InteractiveWorkflow;
-using Microsoft.R.Host.Client.Session;
 using Microsoft.VisualStudio.ProjectSystem;
 using Microsoft.VisualStudio.ProjectSystem.FileSystemMirroring;
 using Microsoft.VisualStudio.R.Package.Commands;
-using Microsoft.VisualStudio.R.Package.Shell;
-using static System.FormattableString;
 #if VS14
 using Microsoft.VisualStudio.ProjectSystem.Designers;
 using Microsoft.VisualStudio.ProjectSystem.Utilities;
@@ -37,7 +33,8 @@ namespace Microsoft.VisualStudio.R.Package.ProjectSystem.Commands {
         }
 
         public Task<CommandStatusResult> GetCommandStatusAsync(IImmutableSet<IProjectTree> nodes, long commandId, bool focused, string commandText, CommandStatus progressiveStatus) {
-            if (commandId == RPackageCommandId.icmdCopyRemoteItemPath && nodes.IsSingleNodePath() && _interactiveWorkflowProvider.GetOrCreate().RSession.IsRemote) {
+            var workflow = _interactiveWorkflowProvider.GetOrCreate();
+            if (commandId == RPackageCommandId.icmdCopyRemoteItemPath && nodes.IsSingleNodePath() && workflow.RSession.IsHostRunning && workflow.RSession.IsRemote) {
                 return Task.FromResult(new CommandStatusResult(true, commandText, CommandStatus.Enabled | CommandStatus.Supported));
             }
             return Task.FromResult(CommandStatusResult.Unhandled);
@@ -51,17 +48,11 @@ namespace Microsoft.VisualStudio.R.Package.ProjectSystem.Commands {
 
             var properties = _configuredProject.Services.ExportProvider.GetExportedValue<ProjectProperties>();
             var path = nodes.GetSingleNodePath();
-
-            string remotePath = await properties.GetRemoteProjectPathAsync();
-            string projectName = properties.GetProjectName();
             var projectRelativePath = _configuredProject.UnconfiguredProject.MakeRelative(path);
-
-            string fullRemotePath = remotePath + projectName + "/" + projectRelativePath;
+            string fullRemotePath = await properties.ToRRemotePathAsync(projectRelativePath);
 
             if (!string.IsNullOrEmpty(fullRemotePath)) {
-                try {
-                    Clipboard.SetData(DataFormats.UnicodeText, Invariant($"\"{fullRemotePath}\""));
-                } catch (ExternalException) { }
+                fullRemotePath.CopyToClipboard();
             }
 
             return true;
