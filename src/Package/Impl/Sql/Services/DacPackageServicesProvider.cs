510a3704a3d8ecd46f43d9ca72003e23f69b4f74
Mikhail Arkhipov <mikhaila@outlook.com>
2016-10-11 10:43:00 -0700

    Avoid exporting services and use reflection instead

--- src/Package/Impl/Sql/Services/DacPackageServicesProvider.cs
+++ src/Package/Impl/Sql/Services/DacPackageServicesProvider.cs
@@ -1,7 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Common.Core;
 using Microsoft.Common.Core.Shell;
 using Microsoft.R.Components.Sql.Publish;
 using Microsoft.VisualStudio.Utilities;
@@ -22,7 +27,7 @@ namespace Microsoft.VisualStudio.R.Package.Sql.Publish {
 
         public IDacPackageServices GetDacPackageServices(bool showMessage = false) {
             if(IsSqlToolsInstalled()) {
-                return _coreShell.ExportProvider.GetExportedValueOrDefault<IDacPackageServices>();
+                return TryGetDacServices();
             } else {
 #if VS14
                 var message = Resources.SqlPublish_NoSqlToolsVS14;
@@ -47,6 +52,14 @@ namespace Microsoft.VisualStudio.R.Package.Sql.Publish {
                 installed = ct != null && ct != _ctrs.UnknownContentType;
             }
             return installed;
+        }
+
+        private IDacPackageServices TryGetDacServices() {
+            var thisAssemblyPath = Assembly.GetExecutingAssembly().GetAssemblyPath();
+            var directory = Path.GetDirectoryName(thisAssemblyPath);
+            var rSqlAssembly = Assembly.LoadFrom(Path.Combine(directory, "Microsoft.VisualStudio.R.Sql.dll"));
+            var type = rSqlAssembly.GetTypes().FirstOrDefault(t => typeof(IDacPackageServices).IsAssignableFrom(t));
+            return (IDacPackageServices)Activator.CreateInstance(type);
         }
     }
 }
--- src/R/Sql/Impl/Publish/DacPackageServices.cs
+++ src/R/Sql/Impl/Publish/DacPackageServices.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System.ComponentModel.Composition;
 using Microsoft.R.Components.Sql.Publish;
 
 namespace Microsoft.VisualStudio.R.Sql.Publish {
-    [Export(typeof(IDacPackageServices))]
     internal sealed class DacPackageServices : IDacPackageServices {
         public IDacPacBuilder GetBuilder() {
             return new DacPacBuilder();
