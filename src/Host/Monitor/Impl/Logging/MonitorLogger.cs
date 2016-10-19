8ef3d2d1cc2a8a4867e870f6c1896e8faa6e4365
Karthik Nadig <kanadig@microsoft.com>
2016-10-19 11:47:23 -0700

    Event logging added.

--- src/Host/Monitor/Impl/Logging/MonitorLogger.cs
+++ src/Host/Monitor/Impl/Logging/MonitorLogger.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
+using System.Diagnostics;
+using System.IO;
 using Microsoft.Common.Core.Disposables;
 using Microsoft.Extensions.Logging;
-using System;
-using System.IO;
 
 namespace Microsoft.R.Host.Monitor.Logging {
     internal sealed class MonitorLogger : ILogger, IDisposable {
@@ -45,6 +46,38 @@ namespace Microsoft.R.Host.Monitor.Logging {
 
                 writer.WriteLine();
                 writer.Flush();
+
+                using (EventLog eventLog = new EventLog("Application")) {
+                    EventLogEntryType logType = GetLogType(logLevel);
+                    eventLog.Source = "Application";
+                    string logMessage;
+                    if (exception != null) {
+                        logMessage = string.Format("[{0:u}] <{1}> ({2}), Message: {3}, Exception: {4}", DateTime.Now, _category, logLevel.ToString()[0], message, exception);
+                    } else {
+                        logMessage = string.Format("[{0:u}] <{1}> ({2}), Message: {3}", DateTime.Now, _category, logLevel.ToString()[0], message);
+                    }
+                    eventLog.WriteEntry(logMessage, logType);
+                }
+            }
+        }
+
+        private EventLogEntryType GetLogType(LogLevel logLevel) {
+            switch (logLevel) {
+                case LogLevel.Trace:
+                    return EventLogEntryType.Information;
+                case LogLevel.Debug:
+                    return EventLogEntryType.Information;
+                case LogLevel.Information:
+                    return EventLogEntryType.Information;
+                case LogLevel.Warning:
+                    return EventLogEntryType.Warning;
+                case LogLevel.Error:
+                    return EventLogEntryType.Error;
+                case LogLevel.Critical:
+                    return EventLogEntryType.Error;
+                case LogLevel.None:
+                default:
+                    return EventLogEntryType.Information;
             }
         }
     }
