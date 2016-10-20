51af6dc275f39424c6d98e750c3373633ff01dbc
Karthik Nadig <kanadig@microsoft.com>
2016-10-20 11:40:08 -0700

    Addressing comments.

--- src/Host/UserProfile/Impl/ServiceLogger.cs
+++ src/Host/UserProfile/Impl/ServiceLogger.cs
@@ -3,14 +3,13 @@
 
 using System;
 using System.Diagnostics;
-using System.IO;
+using System.Globalization;
 using Microsoft.Common.Core.Disposables;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.R.Host.UserProfile {
     internal sealed class ServiceLogger : ILogger, IDisposable {
         private readonly string _category;
-        private object _logLock = new object();
 
         public ServiceLogger(string category) {
             _category = category;
@@ -27,21 +26,19 @@ namespace Microsoft.R.Host.UserProfile {
             return true;
         }
 
-        
+
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
-            lock (_logLock) {
-                string message = formatter(state, exception);
-                using (EventLog eventLog = new EventLog("Application")) {
-                    EventLogEntryType logType = GetLogType(logLevel);
-                    eventLog.Source = "Application";
-                    string logMessage;
-                    if (exception != null) {
-                        logMessage = string.Format("[{0:u}] <{1}> ({2}), Message: {3}, Exception: {4}", DateTime.Now, _category, logLevel.ToString()[0], message, exception);
-                    } else {
-                        logMessage = string.Format("[{0:u}] <{1}> ({2}), Message: {3}", DateTime.Now, _category, logLevel.ToString()[0], message);
-                    }
-                    eventLog.WriteEntry(logMessage, logType);
+            string message = formatter(state, exception);
+            using (EventLog eventLog = new EventLog("Application")) {
+                EventLogEntryType logType = GetLogType(logLevel);
+                eventLog.Source = "Application";
+                string logMessage;
+                if (exception != null) {
+                    logMessage = string.Format(CultureInfo.CurrentCulture, "[{0:u}] <{1}> ({2}):{3}{4}{5}Exception: {6}", DateTime.Now, _category, logLevel.ToString()[0], Environment.NewLine, message, Environment.NewLine, exception);
+                } else {
+                    logMessage = string.Format(CultureInfo.CurrentCulture, "[{0:u}] <{1}> ({2}):{3}{4}", DateTime.Now, _category, logLevel.ToString()[0], Environment.NewLine, message);
                 }
+                eventLog.WriteEntry(logMessage, logType);
             }
         }
 
