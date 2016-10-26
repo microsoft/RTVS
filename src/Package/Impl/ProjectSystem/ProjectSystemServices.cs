7efc6ff910eee8e92350bf6ffb4754e9f3872933
Mikhail Arkhipov <mikhaila@outlook.com>
2016-10-26 10:37:33 -0700

    Fix templates paths on F5

--- src/Package/Impl/ProjectSystem/ProjectSystemServices.cs
+++ src/Package/Impl/ProjectSystem/ProjectSystemServices.cs
@@ -67,60 +67,40 @@ namespace Microsoft.VisualStudio.R.Package.ProjectSystem {
                 var solution = (Solution2)dte.Solution;
 
                 // Construct name of the compressed template
-                var compressedTemplateName = Path.ChangeExtension(templateName, "zip");
-                var templatePath = Path.Combine(GetProjectItemTemplatesFolder(), compressedTemplateName);
+                templateName = Path.ChangeExtension(templateName, "vstemplate");
+                var templatePath = Path.Combine(GetProjectItemTemplatesFolder(), Path.GetFileNameWithoutExtension(templateName), templateName);
 
-                // We will be extracting template contents into a temp folder
-                var uncompressedTemplateFolder = Path.Combine(Path.GetTempPath(), templateName);
-                var uncompressedTemplateName = Path.ChangeExtension(compressedTemplateName, "vstemplate");
-                var tempTemplateFile = Path.Combine(uncompressedTemplateFolder, uncompressedTemplateName);
+                // Given path to the project or a folder in it, generate unique file name
+                var fileName = GetUniqueFileName(destinationPath, name, extension);
 
-                // Extract template files overwriting any existing ones
-                using (ZipArchive zip = ZipFile.OpenRead(templatePath)) {
-                    foreach (ZipArchiveEntry entry in zip.Entries) {
-                        if (!Directory.Exists(uncompressedTemplateFolder)) {
-                            Directory.CreateDirectory(uncompressedTemplateFolder);
-                        }
-                        string destFilePath = Path.Combine(uncompressedTemplateFolder, entry.FullName);
-                        if (!string.IsNullOrEmpty(entry.Name)) {
-                            entry.ExtractToFile(destFilePath, true);
-                        } else {
-                            Directory.CreateDirectory(Path.GetDirectoryName(destFilePath));
-                        }
-                    }
+                // Locate folder in the project
+                var projectFolder = Path.GetDirectoryName(project.FullName);
+                if (destinationPath.StartsWithIgnoreCase(projectFolder)) {
+                    ProjectItems projectItems = project.ProjectItems;
 
-                    // Given path to the project or a folder in it, generate unique file name
-                    var fileName = GetUniqueFileName(destinationPath, name, extension);
+                    if (destinationPath.Length > projectFolder.Length) {
+                        var relativePath = destinationPath.Substring(projectFolder.Length + 1);
 
-                    // Locate folder in the project
-                    var projectFolder = Path.GetDirectoryName(project.FullName);
-                    if (destinationPath.StartsWith(projectFolder, StringComparison.OrdinalIgnoreCase)) {
-                        ProjectItems projectItems = project.ProjectItems;
-
-                        if (destinationPath.Length > projectFolder.Length) {
-                            var relativePath = destinationPath.Substring(projectFolder.Length + 1);
-
-                            // Go into folders and find project item to insert the file in
-                            while (relativePath.Length > 0) {
-                                int index = relativePath.IndexOf('\\');
-                                string folder;
-                                if (index >= 0) {
-                                    folder = relativePath.Substring(0, index);
-                                    relativePath = relativePath.Substring(index + 1);
-                                } else {
-                                    folder = relativePath;
-                                    relativePath = string.Empty;
-                                }
-                                try {
-                                    var item = projectItems.Item(folder);
-                                    projectItems = item.ProjectItems;
-                                } catch (COMException) {
-                                    return;
-                                }
+                        // Go into folders and find project item to insert the file in
+                        while (relativePath.Length > 0) {
+                            int index = relativePath.IndexOf('\\');
+                            string folder;
+                            if (index >= 0) {
+                                folder = relativePath.Substring(0, index);
+                                relativePath = relativePath.Substring(index + 1);
+                            } else {
+                                folder = relativePath;
+                                relativePath = string.Empty;
+                            }
+                            try {
+                                var item = projectItems.Item(folder);
+                                projectItems = item.ProjectItems;
+                            } catch (COMException) {
+                                return;
                             }
                         }
-                        projectItems?.AddFromTemplate(tempTemplateFile, Path.GetFileName(fileName));
                     }
+                    projectItems?.AddFromTemplate(templatePath, Path.GetFileName(fileName));
                 }
             }
         }
@@ -149,7 +129,7 @@ namespace Microsoft.VisualStudio.R.Package.ProjectSystem {
         public string GetProjectItemTemplatesFolder() {
             // In F5 (Experimental instance) scenario templates are deployed where the extension is.
             string assemblyPath = Assembly.GetExecutingAssembly().GetAssemblyPath();
-            var templatesFolder = Path.Combine(Path.GetDirectoryName(assemblyPath), @"ItemTemplates\");
+            var templatesFolder = Path.Combine(Path.GetDirectoryName(assemblyPath), @"Templates\ItemTemplates\");
             if (!Directory.Exists(templatesFolder)) {
                 // Real install scenario, templates are in 
                 // C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\ItemTemplates\R
