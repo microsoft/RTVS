8bc40b7ba0407e5e5f9c363e9a648c4f4e6699e1
Mikhail Arkhipov <mikhaila@microsoft.com>
2016-10-27 14:10:31 -0700

    Update gitignore

--- src/Package/Impl/UpdateReferences.cmd
+++ src/Package/Impl/UpdateReferences.cmd
@@ -1,2 +1,2 @@
-﻿copy /y "%1\project.%2.json" "%1\project.json"
-copy /y "%1\source.extension.%2.vsixmanifest" "%1\source.extension.vsixmanifest"
+﻿copy /y %1\project.%2.json %1\project.json
+copy /y %1\source.extension.%2.vsixmanifest %1\source.extension.vsixmanifest
