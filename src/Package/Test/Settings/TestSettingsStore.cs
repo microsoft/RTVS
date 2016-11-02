51593282d133ce034d362a393164685aa778e053
Mikhail Arkhipov <mikhaila@outlook.com>
2016-11-02 16:39:19 -0700

    Test code

--- src/Package/Test/Settings/TestSettingsStore.cs
+++ src/Package/Test/Settings/TestSettingsStore.cs
@@ -8,73 +8,52 @@ using Microsoft.VisualStudio.Settings;
 
 namespace Microsoft.VisualStudio.R.Package.Test.Settings {
     public sealed class TestSettingsStore : WritableSettingsStore {
+        private readonly Dictionary<string, Dictionary<string, object>> _collections = new Dictionary<string, Dictionary<string, object>>();
+
         public override bool CollectionExists(string collectionPath) {
-            throw new NotImplementedException();
+            return _collections.ContainsKey(collectionPath);
         }
 
         public override void CreateCollection(string collectionPath) {
-            throw new NotImplementedException();
+            if (CollectionExists(collectionPath)) {
+                throw new InvalidOperationException(collectionPath);
+            }
+            _collections[collectionPath] = new Dictionary<string, object>();
         }
 
         public override bool DeleteCollection(string collectionPath) {
-            throw new NotImplementedException();
+            if (CollectionExists(collectionPath)) {
+                _collections.Remove(collectionPath);
+                return true;
+            }
+            return false;
         }
 
         public override bool DeleteProperty(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
+            _collections[collectionPath].Remove(propertyName);
+            return true;
         }
 
-        public override bool GetBoolean(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override bool GetBoolean(string collectionPath, string propertyName, bool defaultValue) {
-            throw new NotImplementedException();
-        }
-
-        public override int GetInt32(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override int GetInt32(string collectionPath, string propertyName, int defaultValue) {
-            throw new NotImplementedException();
-        }
-
-        public override long GetInt64(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override long GetInt64(string collectionPath, string propertyName, long defaultValue) {
-            throw new NotImplementedException();
-        }
-
-        public override DateTime GetLastWriteTime(string collectionPath) {
-            throw new NotImplementedException();
-        }
-
+        public override bool GetBoolean(string collectionPath, string propertyName) => GetValue<bool>(collectionPath, propertyName);
+        public override bool GetBoolean(string collectionPath, string propertyName, bool defaultValue) => GetValue<bool>(collectionPath, propertyName, defaultValue);
+        public override int GetInt32(string collectionPath, string propertyName) => GetValue<int>(collectionPath, propertyName);
+        public override int GetInt32(string collectionPath, string propertyName, int defaultValue) => GetValue<int>(collectionPath, propertyName, defaultValue);
+        public override long GetInt64(string collectionPath, string propertyName) => GetValue<long>(collectionPath, propertyName);
+        public override long GetInt64(string collectionPath, string propertyName, long defaultValue) => GetValue<long>(collectionPath, propertyName, defaultValue);
+        public override DateTime GetLastWriteTime(string collectionPath) => DateTime.Now;
         public override MemoryStream GetMemoryStream(string collectionPath, string propertyName) {
             throw new NotImplementedException();
         }
 
-        public override int GetPropertyCount(string collectionPath) {
-            throw new NotImplementedException();
-        }
-
-        public override IEnumerable<string> GetPropertyNames(string collectionPath) {
-            throw new NotImplementedException();
-        }
+        public override int GetPropertyCount(string collectionPath) => _collections[collectionPath].Keys.Count;
+        public override IEnumerable<string> GetPropertyNames(string collectionPath) => _collections[collectionPath].Keys;
 
         public override SettingsType GetPropertyType(string collectionPath, string propertyName) {
             throw new NotImplementedException();
         }
 
-        public override string GetString(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override string GetString(string collectionPath, string propertyName, string defaultValue) {
-            throw new NotImplementedException();
-        }
+        public override string GetString(string collectionPath, string propertyName) => GetValue<string>(collectionPath, propertyName);
+        public override string GetString(string collectionPath, string propertyName, string defaultValue)=> GetValue<string>(collectionPath, propertyName, defaultValue);
 
         public override int GetSubCollectionCount(string collectionPath) {
             throw new NotImplementedException();
@@ -84,25 +63,11 @@ namespace Microsoft.VisualStudio.R.Package.Test.Settings {
             throw new NotImplementedException();
         }
 
-        public override uint GetUInt32(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override uint GetUInt32(string collectionPath, string propertyName, uint defaultValue) {
-            throw new NotImplementedException();
-        }
-
-        public override ulong GetUInt64(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
-
-        public override ulong GetUInt64(string collectionPath, string propertyName, ulong defaultValue) {
-            throw new NotImplementedException();
-        }
-
-        public override bool PropertyExists(string collectionPath, string propertyName) {
-            throw new NotImplementedException();
-        }
+        public override uint GetUInt32(string collectionPath, string propertyName) => GetValue<uint>(collectionPath, propertyName);
+        public override uint GetUInt32(string collectionPath, string propertyName, uint defaultValue) => GetValue<uint>(collectionPath, propertyName, defaultValue);
+        public override ulong GetUInt64(string collectionPath, string propertyName) => GetValue<ulong>(collectionPath, propertyName);
+        public override ulong GetUInt64(string collectionPath, string propertyName, ulong defaultValue) => GetValue<ulong>(collectionPath, propertyName, defaultValue);
+        public override bool PropertyExists(string collectionPath, string propertyName) => _collections[collectionPath].ContainsKey(propertyName);
 
         public override void SetBoolean(string collectionPath, string propertyName, bool value) {
             throw new NotImplementedException();
@@ -130,6 +95,19 @@ namespace Microsoft.VisualStudio.R.Package.Test.Settings {
 
         public override void SetUInt64(string collectionPath, string propertyName, ulong value) {
             throw new NotImplementedException();
+        }
+
+        private T GetValue<T>(string collectionPath, string propertyName) {
+            object value;
+            _collections[collectionPath].TryGetValue(propertyName, out value);
+            return (T)value;
+        }
+
+        private T GetValue<T>(string collectionPath, string propertyName, T defaultValue) {
+            if (_collections[collectionPath].ContainsKey(propertyName)) {
+                return GetValue<T>(collectionPath, propertyName);
+            }
+            return defaultValue;
         }
     }
 }
