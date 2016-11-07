a4bbdd0122d13947a7d02304b8c7ecc93f0b691b
Hugues Valois <hugues_valois@hotmail.com>
2016-11-07 11:15:17 -0800

    Prefer opening local documentation for active connection if it is available.

--- src/R/Components/Impl/Documentation/OnlineDocumentationUrls.cs
+++ src/R/Components/Impl/Documentation/OnlineDocumentationUrls.cs
@@ -1,20 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Microsoft.VisualStudio.R.Package.Documentation {
-    internal static class DocumentationUrls {
-        public const string RtvsDocumentation = "http://aka.ms/rtvs-docs";
-        public const string RtvsSamples = "http://aka.ms/rtvs-examples";
+namespace Microsoft.R.Components.Documentation {
+    public static class OnlineDocumentationUrls {
         public const string CranIntro = "http://aka.ms/rtvs-cran-r-intro";
-        public const string CranViews = "http://aka.ms/rtvs-cran-views";
         public const string CranData = "http://aka.ms/rtvs-cran-data";
         public const string CranExtensions = "http://aka.ms/rtvs-cran-r-exts";
+        public const string CranViews = "http://aka.ms/rtvs-cran-views";
+        public const string RtvsDocumentation = "http://aka.ms/rtvs-docs";
+        public const string RtvsSamples = "http://aka.ms/rtvs-examples";
         public const string CheckForRtvsUpdates = "http://aka.ms/rtvs-checkforupdate";
         public const string MicrosoftRProducts = "http://aka.ms/rtvs-msft-r";
     }
