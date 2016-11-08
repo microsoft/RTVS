microsoft/RTVS

commit 9fca44e26f8c79499c6762da5360513b5b8f8c69

Fix #2644: Package manager RHost session should use the name PackageManager

src/Host/Client/Impl/Session/RSessionProvider.cs
@@ -61,7 +61,7 @@ namespace Microsoft.R.Host.Client.Session {
 
                 try {
                     await session.StartHostAsync(new RHostStartupInfo {
-                        Name = "IsolatedRHost" + session.Id,
+                        Name = "Isolated_" + (startupInfo.Name ?? session.Id.ToString()),
                         CranMirrorName = startupInfo.CranMirrorName,
                         CodePage = startupInfo.CodePage
                     }, null, cancellationToken: cancellationToken);

src/R/Components/Impl/PackageManager/Implementation/RPackageManager.cs
@@ -138,6 +138,7 @@ namespace Microsoft.R.Components.PackageManager.Implementation {
             // separate package query session to avoid freezing the REPL.
             try {
                 var startupInfo = new RHostStartupInfo {
+                    Name = "PackageManager",
                     CranMirrorName = _settings.CranMirror,
                     CodePage = _settings.RCodePage
                 };

commit c2c8a0742483aea4846260652c41cc2f3df1cef3

Fix regression introduced by 9fca44e (Fix #2644: Package manager RHost session should use the name PackageManager)

src/Host/Client/Impl/Session/RSessionProvider.cs
@@ -61,7 +61,7 @@ namespace Microsoft.R.Host.Client.Session {
 
                 try {
                     await session.StartHostAsync(new RHostStartupInfo {
-                        Name = "Isolated_" + (startupInfo.Name ?? session.Id.ToString()),
+                        Name = $"Isolated_{startupInfo.Name ?? string.Empty}_{session.Id}",
                         CranMirrorName = startupInfo.CranMirrorName,
                         CodePage = startupInfo.CodePage
                     }, null, cancellationToken: cancellationToken);

src/Host/Client/Test/Session/RSessionProviderTest.cs
@@ -58,6 +58,24 @@ namespace Microsoft.R.Host.Client.Test.Session {
         }
 
         [Test]
+        public async Task BeginEvaluationAsync_ParallelAccess() {
+           using (var sessionProvider = new RSessionProvider(TestCoreServices.CreateReal())) {
+                await sessionProvider.TrySwitchBrokerAsync(nameof(RSessionProviderTest) + nameof(BeginEvaluationAsync_ParallelAccess));
+                var tasks = await ParallelTools.InvokeAsync(10, async i => {
+                    var startupInfo = new RHostStartupInfo {
+                        Name = nameof(BeginEvaluationAsync_ParallelAccess)
+                    };
+
+                    using (var evaluation = await sessionProvider.BeginEvaluationAsync(startupInfo)) {
+                        return await evaluation.EvaluateAsync<int>($"{i} + 2", REvaluationKind.Normal);
+                    }
+                });
+
+               tasks.Select(t => t.Result).Should().Contain(Enumerable.Range(2, 10));
+           }
+        }
+
+        [Test]
         public async Task ConnectWhenSwitching() {
             using (var sessionProvider = new RSessionProvider(TestCoreServices.CreateReal())) {
                 var guid1 = new Guid();