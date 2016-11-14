25a54501f3d57b5c6ff71a02a61162fc773cde2a
MikhailArkhipov <mikhaila@microsoft.com>
2016-11-14 11:03:40 -0800

    host load reporting

---
+++ src/Host/Broker/Impl/About/LoadController.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Management;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.R.Host.Broker.Security;
+using Microsoft.R.Host.Broker.Sessions;
+using Microsoft.R.Host.Protocol;
+
+namespace Microsoft.R.Host.Broker.About {
+    [Authorize(Policy = Policies.RUser)]
+    [Route("/load")]
+    public class LoadController : Controller {
+        private readonly PerformanceCounter _cpuCounter = new PerformanceCounter("Performance", "CPU");
+        private readonly PerformanceCounter _networkCounter = new PerformanceCounter("Performance", "Network");
+
+        public LoadController(SessionManager sessionManager) {
+        }
+
+        [HttpGet]
+        public HostLoad Get() {
+            var h = new HostLoad();
+
+            var query = new SelectQuery(@"Select * from Win32_OperatingSystem");
+            using (var search = new ManagementObjectSearcher(query)) {
+                foreach (var mo in search.Get()) {
+                    h.FreeVirtualMemory = GetSizeInGB(mo, "FreeVirtualMemory");
+                    h.FreePhysicalMemory = GetSizeInGB(mo, "FreePhysicalMemory");
+                    break;
+                }
+            }
+
+            h.CpuLoad = _cpuCounter.NextValue();
+            h.NetworkLoad = _networkCounter.NextValue();
+
+            return h;
+        }
+
+        private long GetSizeInGB(ManagementBaseObject mo, string name) {
+            int result;
+            var x = mo[name].ToString();
+            return Int32.TryParse(x, out result) ? result / 1024 : 0;
+        }
+    }
+}
---
+++ src/Host/Protocol/Impl/HostLoad.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.R.Host.Protocol {
+    public class HostLoad {
+        public long FreeVirtualMemory { get; set; }
+        public long FreePhysicalMemory { get; set; }
+        public float CpuLoad { get; set; }
+        public float NetworkLoad { get; set; }
+    }
+}
