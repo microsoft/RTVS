899c73bc1bdddd49e0f57b36185f3be67d960fd7
Karthik Nadig <kanadig@microsoft.com>
2016-11-29 11:08:45 -0800

    Adding sign in skip paths to hash set.

--- src/Host/Broker/Impl/Extensions/BasicSignInContextExtensions.cs
+++ src/Host/Broker/Impl/Extensions/BasicSignInContextExtensions.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
 using Odachi.AspNetCore.Authentication.Basic;
 
 namespace Microsoft.R.Host.Broker {
     public static class BasicSignInContextExtensions {
+
+        static readonly HashSet<string> _skipSignInPaths = new HashSet<string>() { "/info/load", "/info/about" };
         public static bool IsSignInRequired(this BasicSignInContext context) {
             string path = context.HttpContext.Request.Path.ToString();
-            if (path.Equals("/info/load") || path.Equals("/info/about")) {
+            if (_skipSignInPaths.Contains(path)) {
                 return false;
             }
             return true;
