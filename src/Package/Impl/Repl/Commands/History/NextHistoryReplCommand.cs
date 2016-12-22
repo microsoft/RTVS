f258124c6fa7ea4f4b4a8777a5b8ec27938b80fb
Mikhail Arkhipov <mikhaila@outlook.com>
2016-12-22 10:25:21 -0800

    Fix sync between history window and REPL

--- src/Package/Impl/Repl/Commands/History/NextHistoryReplCommand.cs
+++ src/Package/Impl/Repl/Commands/History/NextHistoryReplCommand.cs
@@ -1,14 +1,19 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Microsoft.R.Components.History;
 using Microsoft.R.Components.InteractiveWorkflow;
 using Microsoft.VisualStudio.R.Package.Commands;
 
 namespace Microsoft.VisualStudio.R.Package.Repl.Commands {
     internal sealed class NextHistoryReplCommand : ReplCommandBase {
-        public NextHistoryReplCommand(IRInteractiveWorkflow interactiveWorkflow) :
-            base(interactiveWorkflow, RPackageCommandId.icmdNextHistoryRepl) { }
+        private readonly IRHistory _history;
 
-        protected override void DoOperation() => Workflow.ActiveWindow.InteractiveWindow.Operations.HistoryNext();
+        public NextHistoryReplCommand(IRInteractiveWorkflow interactiveWorkflow) :
+            base(interactiveWorkflow, RPackageCommandId.icmdNextHistoryRepl) {
+            _history = interactiveWorkflow.History;
+        }
+
+        protected override void DoOperation() => _history.NextEntry();
     }
 }
