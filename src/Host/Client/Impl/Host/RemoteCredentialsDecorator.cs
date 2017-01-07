0b048b5171e97524fdff4e3c499197f9897ee671
Karthik Nadig <karthik.nadig@live.com>
2017-01-07 12:58:41 -0800

    Credential Prompt updated to CredUIPromptForWindowsCredentials

--- src/Host/Client/Impl/Host/RemoteCredentialsDecorator.cs
+++ src/Host/Client/Impl/Host/RemoteCredentialsDecorator.cs
@@ -16,8 +16,7 @@ namespace Microsoft.R.Host.Client.Host {
     internal class RemoteCredentialsDecorator : ICredentialsDecorator {
         private readonly ISecurityService _securityService;
         private readonly IMainThread _mainThread;
-        private readonly Credentials _credentials = new Credentials();
-        private readonly AutoResetEvent _credentialsValidated = new AutoResetEvent(true);
+        private Credentials _credentials;
         private readonly string _authority;
         private readonly AsyncReaderWriterLock _lock;
         private bool _credentialsAreValid;
@@ -30,7 +29,7 @@ namespace Microsoft.R.Host.Client.Host {
             _credentialsAreValid = true;
         }
 
-        public NetworkCredential GetCredential(Uri uri, string authType) => new NetworkCredential(_credentials.UserName, _credentials.Password);
+        public NetworkCredential GetCredential(Uri uri, string authType) => new NetworkCredential(_credentials?.UserName, _credentials?.Password);
 
         public async Task<IDisposable> LockCredentialsAsync(CancellationToken cancellationToken = default(CancellationToken)) {
             // If there is already a LockCredentialsAsync request for which there hasn't been a validation yet, wait until it completes.
@@ -40,32 +39,25 @@ namespace Microsoft.R.Host.Client.Host {
 
             await _mainThread.SwitchToAsync(cancellationToken);
 
-            Credentials credentials;
             try {
-                var invalidateStoredCredentials = !Volatile.Read(ref _credentialsAreValid);
-                credentials = await _securityService.GetUserCredentialsAsync(_authority, invalidateStoredCredentials, cancellationToken);
+                _credentials = await _securityService.GetUserCredentialsAsync(_authority, cancellationToken);
             } catch (Exception) {
                 token.Dispose();
                 throw;
             }
 
-            _credentials.UserName = credentials.UserName;
-            _credentials.Password = credentials.Password;
             Volatile.Write(ref _credentialsAreValid, true);
+            SecurityUtilities.WriteCredentials(_authority, _credentials);
 
             return Disposable.Create(() => {
-                CredUIConfirmCredentials(_authority, Volatile.Read(ref _credentialsAreValid));
                 token.Dispose();
             });
         }
 
         public void InvalidateCredentials() {
             Volatile.Write(ref _credentialsAreValid, false);
+            SecurityUtilities.DeleteCredentials(_authority);
         }
 
-        public void OnCredentialsValidated(bool isValid) {
-            CredUIConfirmCredentials(_authority, isValid);
-            _credentialsValidated.Set();
-        }
     }
 }
--- src/Host/Client/Impl/NativeMethods.cs
+++ src/Host/Client/Impl/NativeMethods.cs
@@ -18,7 +18,47 @@ namespace Microsoft.R.Host.Client {
         [DllImport("kernel32.dll")]
         public static extern int GetOEMCP();
 
-        [DllImport("credui", CharSet = CharSet.Auto)]
-        public static extern int CredUIConfirmCredentials(string pszTargetName, bool bConfirm);
+        public enum CRED_TYPE {
+            GENERIC = 1,
+            DOMAIN_PASSWORD = 2,
+            DOMAIN_CERTIFICATE = 3,
+            DOMAIN_VISIBLE_PASSWORD = 4,
+            MAXIMUM = 5
+        }
+
+        public enum CRED_PERSIST : uint {
+            CRED_PERSIST_SESSION = 1,
+            CRED_PERSIST_LOCAL_MACHINE = 2,
+            CRED_PERSIST_ENTERPRISE = 3
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct CredentialData {
+            public uint flags;
+            public uint type;
+            public string targetName;
+            public string comment;
+            public System.Runtime.InteropServices.ComTypes.FILETIME lastWritten; // .NET 2.0
+            public uint credentialBlobSize;
+            public IntPtr credentialBlob;
+            public uint persist;
+            public uint attributeCount;
+            public IntPtr credAttribute;
+            public string targetAlias;
+            public string userName;
+        }
+
+        [DllImport("Advapi32.dll", SetLastError = true, EntryPoint = "CredWriteW", CharSet = CharSet.Unicode)]
+        public static extern bool CredWrite(ref CredentialData userCredential, uint flags);
+
+        [DllImport("Advapi32.dll", EntryPoint = "CredReadW", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CredRead(
+            string target,
+            CRED_TYPE type,
+            int reservedFlag,
+            out CredentialData userCredential);
+
+        [DllImport("advapi32.dll", EntryPoint = "CredDeleteW", CharSet = CharSet.Unicode)]
+        public static extern bool CredDelete(string target, CRED_TYPE type, int flags);
     }
 }
