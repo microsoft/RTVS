9ad15f14e75633a10f5e991febbeaa824a349be4
Alexander Sher <alsher@microsoft.com>
2017-01-19 12:18:11 -0600

    Add TestSecurityService

---
+++ src/Common/Core/Test/Fakes/Shell/TestSecurityService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Common.Core.Security;
+
+namespace Microsoft.Common.Core.Test.Fakes.Shell {
+    public class TestSecurityService : ISecurityService {
+        public Task<Credentials> GetUserCredentialsAsync(string authority, string workspaceName, CancellationToken cancellationToken = new CancellationToken()) {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> ValidateX509CertificateAsync(X509Certificate certificate, string message, CancellationToken cancellationToken = new CancellationToken()) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteUserCredentials(string authority) => true;
+    }
+}
