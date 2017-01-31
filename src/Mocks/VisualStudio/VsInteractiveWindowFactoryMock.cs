bde79b768060c5ac65a7125103c9c1844dfb67e7
Alexander Sher <alsher@microsoft.com>
2017-01-31 14:30:28 -0600

    Fix ReplCommandTest.SendToReplTest

--- src/Mocks/VisualStudio/VsInteractiveWindowFactoryMock.cs
+++ src/Mocks/VisualStudio/VsInteractiveWindowFactoryMock.cs
@@ -7,15 +7,18 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.Editor.Mocks;
 using Microsoft.VisualStudio.InteractiveWindow;
 using Microsoft.VisualStudio.InteractiveWindow.Shell;
+using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Microsoft.VisualStudio.Shell.Mocks {
-
     [ExcludeFromCodeCoverage]
     [Export(typeof(IVsInteractiveWindowFactory))]
-    public sealed class VsInteractiveWindowFactoryMock : IVsInteractiveWindowFactory {
-        public IVsInteractiveWindow Create(Guid providerId, int instanceId, string title, IInteractiveEvaluator evaluator, __VSCREATETOOLWIN creationFlags = 0) {
-            return new VsInteractiveWindowMock(new WpfTextViewMock(new TextBufferMock(string.Empty, "R")), evaluator);
-        }
+    public sealed class VsInteractiveWindowFactoryMock : IVsInteractiveWindowFactory2 {
+        public IVsInteractiveWindow Create(Guid providerId, int instanceId, string title, IInteractiveEvaluator evaluator, __VSCREATETOOLWIN creationFlags = 0) 
+            => new VsInteractiveWindowMock(new WpfTextViewMock(new TextBufferMock(string.Empty, "R")), evaluator);
+
+        public IVsInteractiveWindow Create(Guid providerId, int instanceId, string title, IInteractiveEvaluator evaluator, __VSCREATETOOLWIN creationFlags, Guid toolbarCommandSet, uint toolbarId,
+            IOleCommandTarget toolbarCommandTarget)
+            => new VsInteractiveWindowMock(new WpfTextViewMock(new TextBufferMock(string.Empty, "R")), evaluator);
     }
 }
