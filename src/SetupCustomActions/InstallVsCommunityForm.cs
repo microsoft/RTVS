076a2c8fec34d7d0acc865a59ebe09d797b4b5fd
Alexander Sher <alsher@microsoft.com>
2017-03-12 20:13:07 -0500

    - Remove VS14 Setup - Rename Microsoft.Windows.Core to Microsoft.Rtvs.Core.Windows

--- src/SetupCustomActions/InstallVsCommunityForm.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-namespace SetupCustomActions {
-    public partial class InstallVsCommunityForm : YesNoMessageBox {
-        public InstallVsCommunityForm() : base(Resources.VsCommunityQuestion) {
-        }
-    }
-}
--- src/Windows/Core/Impl/Resources.Designer.cs
+++ src/Windows/Core/Impl/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Windows.Core {
+namespace Microsoft.Common.Core {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.Windows.Core {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Windows.Core.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Common.Core.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
