b91d12ee14916cf4bc9dbab6fe5d214578e0e696
Alexander Sher <alsher@microsoft.com>
2017-03-21 07:37:58 -0500

    Fix #3322: ServiceManager doesn't detect reentrancy

---
+++ src/Common/Core/Test/Services/ServiceManagerTests.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Common.Core.Services;
+using Microsoft.UnitTests.Core.FluentAssertions;
+using Microsoft.UnitTests.Core.XUnit;
+
+namespace Microsoft.Common.Core.Test.Services {
+    public class ServiceManagerTests {
+        private readonly ServiceManager _serviceManager;
+
+        public ServiceManagerTests() {
+            _serviceManager = new ServiceManager();
+        }
+
+        [Test]
+        public void GetAccessToNonCreatedService_Recursion() {
+            _serviceManager
+                .AddService<I1>(() => {
+                    var i2 = _serviceManager.GetService<I2>();
+                    return new C1();
+                })
+                .AddService<I2>(() => {
+                    var i1 = _serviceManager.GetService<I1>();
+                    return new C2();
+                });
+
+            Action a = () => _serviceManager.GetService<I1>();
+            a.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Test]
+        public void ServiceOfTypeLazyObject() {
+            _serviceManager
+                .AddService(new Lazy<object>())
+                .AddService<object>();
+
+            _serviceManager.GetService<Lazy<object>>().Should().NotBeNull();
+            _serviceManager.GetService<object>().Should().NotBeNull();
+        }
+
+        [Test]
+        public void DoubleAdd() {
+            _serviceManager.AddService<I1>(new C1());
+            
+            Action a = () => _serviceManager.AddService<I1>(new C1());
+            a.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Test]
+        public void Disposed() {
+            _serviceManager.AddService<I1>(new C1());
+            _serviceManager.Dispose();
+
+            Action a = () => _serviceManager.AddService<I1>(new C1());
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.AddService<I2>(new C2());
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.AddService<I2>(() => new C2());
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.GetService<I1>();
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.GetService<I2>();
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => { var l = _serviceManager.GetServices<I1>().ToList(); };
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => { var l = _serviceManager.GetServices<I2>().ToList(); };
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.RemoveService<I1>();
+            a.ShouldThrow<ObjectDisposedException>();
+
+            a = () => _serviceManager.RemoveService<I2>();
+            a.ShouldThrow<ObjectDisposedException>();
+        }
+
+        private interface I1 {}
+        private interface I2 {}
+
+        private class C1 : I1 {}
+        private class C2 : I2 {}
+    }
+}
