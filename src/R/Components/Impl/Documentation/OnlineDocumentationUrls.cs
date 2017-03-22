2752e969c05c7eae3522f809622a2e332230e53d
Mikhail Arkhipov <mikhaila@outlook.com>
2017-03-22 13:14:58 -0700

    Replace links by https

--- src/R/Components/Impl/Documentation/OnlineDocumentationUrls.cs
+++ src/R/Components/Impl/Documentation/OnlineDocumentationUrls.cs
@@ -3,13 +3,13 @@
 
 namespace Microsoft.R.Components.Documentation {
     public static class OnlineDocumentationUrls {
-        public const string CranIntro = "http://aka.ms/rtvs-cran-r-intro";
-        public const string CranData = "http://aka.ms/rtvs-cran-data";
-        public const string CranExtensions = "http://aka.ms/rtvs-cran-r-exts";
-        public const string CranViews = "http://aka.ms/rtvs-cran-views";
-        public const string RtvsDocumentation = "http://aka.ms/rtvs-docs";
-        public const string RtvsSamples = "http://aka.ms/rtvs-examples";
-        public const string CheckForRtvsUpdates = "http://aka.ms/rtvs-checkforupdate";
-        public const string MicrosoftRProducts = "http://aka.ms/rtvs-msft-r";
+        public const string CranIntro = "https://aka.ms/rtvs-cran-r-intro";
+        public const string CranData = "https://aka.ms/rtvs-cran-data";
+        public const string CranExtensions = "https://aka.ms/rtvs-cran-r-exts";
+        public const string CranViews = "https://aka.ms/rtvs-cran-views";
+        public const string RtvsDocumentation = "https://aka.ms/rtvs-docs";
+        public const string RtvsSamples = "https://aka.ms/rtvs-examples";
+        public const string CheckForRtvsUpdates = "https://aka.ms/rtvs-checkforupdate";
+        public const string MicrosoftRProducts = "https://aka.ms/rtvs-msft-r";
     }
 }
