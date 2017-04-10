724a8855353054de1f077bc5d74b4f8903661755
Mikhail Arkhipov <mikhaila@outlook.com>
2017-04-09 22:10:04 -0700

    Provide editor settings in test

--- src/Package/Test/Shell/VsAppShellTestSetup.cs
+++ src/Package/Test/Shell/VsAppShellTestSetup.cs
@@ -14,6 +14,8 @@ using Microsoft.Common.Core.Shell;
 using Microsoft.Common.Core.Test.Fakes.Shell;
 using Microsoft.Common.Core.Test.Logging;
 using Microsoft.Common.Core.Test.Stubs.Shell;
+using Microsoft.Language.Editor.Test.Settings;
+using Microsoft.R.Editor.Settings;
 using Microsoft.R.Support.Test.Utility;
 using Microsoft.UnitTests.Core.Threading;
 using Microsoft.VisualStudio.ComponentModelHost;
@@ -63,6 +65,7 @@ namespace Microsoft.VisualStudio.R.Package.Test.Shell {
                 .AddService(new TestTaskService())
                 .AddService(new TestPlatformServices())
                 .AddService(new TestRToolsSettings())
+                .AddService(new REditorSettings(new TestSettingsStorage()))
                 // OLE and VS specifics
                 .AddService(new VsRegisterProjectGeneratorsMock(), typeof(SVsRegisterProjectTypes))
                 .AddService(VsRegisterEditorsMock.Create(), typeof(SVsRegisterEditors))
