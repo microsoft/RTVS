5b7b8e98e8e9806ec571e86a8a4a2aa708fb18c6
Alexander Sher <alsher@microsoft.com>
2017-04-28 16:00:12 -0700

    Fix #3460: Cannot open variable explorer, VS 2017.2

--- src/Package/Impl/DataInspect/VariableView.xaml.cs
+++ src/Package/Impl/DataInspect/VariableView.xaml.cs
@@ -50,6 +50,7 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect {
 
             InitializeComponent();
             SetImageBackground();
+            FocusManager.SetFocusedElement(this, RootTreeGrid);
 
             _aggregator = _shell.ExportProvider.GetExportedValue<IObjectDetailsViewerAggregator>();
             SetRootNode(VariableViewModel.Ellipsis);
@@ -234,7 +235,7 @@ namespace Microsoft.VisualStudio.R.Package.DataInspect {
             if (focus != null) {
                 var pt = focus.PointToScreen(new Point(1, 1));
                 _shell.ShowContextMenu(
-                    new CommandID(RGuidList.RCmdSetGuid, (int)RContextMenuId.VariableExplorer), (int)pt.X, (int)pt.Y, this);
+                    new CommandID(RGuidList.RCmdSetGuid, (int)RContextMenuId.VariableExplorer), (int)pt.X, (int)pt.Y);
             }
         }
 
