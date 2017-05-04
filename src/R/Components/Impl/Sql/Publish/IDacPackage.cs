18400506560d57ffe84788c87ef7175462ab7bfd
Mikhail Arkhipov <mikhaila@outlook.com>
2017-05-04 10:41:40 -0700

    Dispose dacpac

--- src/R/Components/Impl/Sql/Publish/IDacPackage.cs
+++ src/R/Components/Impl/Sql/Publish/IDacPackage.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
+
 namespace Microsoft.R.Components.Sql.Publish {
     /// <summary>
     /// Represents DAC package
     /// </summary>
-    public interface IDacPackage {
+    public interface IDacPackage: IDisposable {
         void Deploy(string connectionString, string databaseName);
     }
 }
