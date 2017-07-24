8d7d67a17045f52989afa7fccaa8805cd1de4cc9
MikhailArkhipov <mikhaila@microsoft.com>
2017-07-24 11:38:01 -0700

    Restore lost assembly info Update baselines

---
+++ src/Windows/Host/Client/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.R.Host.Client.Test.Fixtures;
+using Microsoft.UnitTests.Core.XUnit;
+
+[assembly: TestFrameworkOverride]
+[assembly: AssemblyFixtureImport(typeof(HostClientServicesFixture), typeof(RemoteBrokerFixture))]
