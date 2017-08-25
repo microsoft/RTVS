3e483b305ab922255cf9e2ae662ce154b220e476
Karthik Nadig <kanadig@microsoft.com>
2017-08-25 09:58:53 -0700

    Switching to Registry mock from core.test

--- src/Windows/Containers/Test/WindowsDockerFinderTests.cs
+++ src/Windows/Containers/Test/WindowsDockerFinderTests.cs
@@ -6,8 +6,11 @@ using FluentAssertions;
 using Microsoft.Common.Core.IO;
 using Microsoft.Common.Core.OS;
 using Microsoft.Common.Core.Services;
+using Microsoft.Common.Core.Test.Registry;
+using Microsoft.Common.Core.Test.StubBuilders;
 using Microsoft.R.Containers.Docker;
 using Microsoft.UnitTests.Core.XUnit;
+using NSubstitute;
 
 namespace Microsoft.R.Containers.Windows.Test {
     [ExcludeFromCodeCoverage]
@@ -15,10 +18,11 @@ namespace Microsoft.R.Containers.Windows.Test {
     public class WindowsDockerFinderTests {
         [Test]
         public void FindDockerInstallationTest() {
-            IServiceManager services= new ServiceManager()
-                .AddService<IFileSystem, FileSystem>()
+            var services = new ServiceManager()
+                .AddService(GetFileSystemMock())
                 .AddService<IProcessServices, ProcessServices>()
-                .AddService<IRegistry, RegistryImpl>();
+                .AddService(new RegistryMock(GetDefaultInstallKey(), GetServiceInstallKey()));
+
             var finder = new WindowsLocalDockerFinder(services);
             var docker = finder.GetLocalDocker();
             docker.Should().NotBeNull();
@@ -28,10 +32,10 @@ namespace Microsoft.R.Containers.Windows.Test {
 
         [Test]
         public void FindDockerInstallationUsingServiceTest() {
-            IServiceManager services = new ServiceManager()
-                .AddService<IFileSystem, FileSystem>()
+            var services = new ServiceManager()
+                .AddService(GetFileSystemMock())
                 .AddService<IProcessServices, ProcessServices>()
-                .AddService(new RegistryMock(new string[] { WindowsLocalDockerFinder.DockerRegistryPath }));
+                .AddService(new RegistryMock(GetServiceInstallKey()));
 
             var finder = new WindowsLocalDockerFinder(services);
             var docker = finder.GetLocalDocker();
@@ -42,16 +46,36 @@ namespace Microsoft.R.Containers.Windows.Test {
 
         [Test]
         public void FindDockerInstallationUsingProgramFilesTest() {
-            IServiceManager services = new ServiceManager()
-                .AddService<IFileSystem, FileSystem>()
+            var services = new ServiceManager()
+                .AddService(GetFileSystemMock())
                 .AddService<IProcessServices, ProcessServices>()
-                .AddService(new RegistryMock(new string[] { WindowsLocalDockerFinder.DockerRegistryPath, WindowsLocalDockerFinder.DockerRegistryPath2 }));
+                .AddService(new RegistryMock());
 
             var finder = new WindowsLocalDockerFinder(services);
             var docker = finder.GetLocalDocker();
             docker.Should().NotBeNull();
             docker.BinPath.Should().NotBeEmpty();
             docker.DockerCommandPath.Should().NotBeEmpty();
+        }
+
+        private RegistryKeyMock GetDefaultInstallKey() {
+            string[] valueNames = { "BinPath", "Version" };
+            string[] values = { "\"C:\\Program Files\\Docker\\Docker\\resources\\bin\"" , "1.0.0.0" };
+            var v1 = new RegistryKeyMock("1.0", null, valueNames, values);
+            return new RegistryKeyMock(@"SOFTWARE\Docker Inc.\Docker", v1);
+        }
+
+        private RegistryKeyMock GetServiceInstallKey() {
+            string[] valueNames = { "ImagePath" };
+            string[] values = { "\"C:\\Program Files\\Docker\\Docker\\com.docker.service\"" };
+            return new RegistryKeyMock(@"SYSTEM\CurrentControlSet\Services\com.docker.service", null, valueNames, values);
+        }
+
+        private IFileSystem GetFileSystemMock() {
+            string path = @"C:\Program Files\Docker\Docker\resources\bin\docker.exe";
+            var fs = Substitute.For<IFileSystem>();
+            fs.FileExists(path).Returns(true);
+            return fs;
         }
     }
 }
