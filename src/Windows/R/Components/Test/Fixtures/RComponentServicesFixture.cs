d568b82a35d7aa02410abfddfb40af24718f34dc
MikhailArkhipov <mikhaila@microsoft.com>
2017-11-09 13:40:12 -0800

    Add MEF components to test fixture

--- src/Windows/R/Components/Test/Fixtures/RComponentServicesFixture.cs
+++ src/Windows/R/Components/Test/Fixtures/RComponentServicesFixture.cs
@@ -27,7 +27,8 @@ namespace Microsoft.R.Components.Test.Fixtures {
             "Microsoft.VisualStudio.Platform.VSEditor.dll",
             "Microsoft.R.Components.dll",
             "Microsoft.R.Components.Windows.dll",
-            "Microsoft.R.Components.Test.dll"
+            "Microsoft.R.Components.Test.dll",
+            "Microsoft.R.Components.Windows.Test.dll"
         };
 
         protected override void SetupServices(IServiceManager serviceManager, ITestInput testInput) {
