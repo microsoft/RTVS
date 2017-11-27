6da869b3faa4f72dabf9a4cafc2e40fafb8d4b29
MikhailArkhipov <mikhaila@microsoft.com>
2017-11-27 15:36:00 -0800

    Attempt to suppress signing

--- src/VsCode/Extension/publish.ps1
+++
@@ -1,4 +0,0 @@
-﻿set DOTNET_PUBLISH=1
-dotnet publish --framework netcoreapp2.0 ..\..\Unix\Host\Broker\Impl\Microsoft.R.Host.Broker.Unix.csproj
-dotnet publish --framework netcoreapp2.0 ..\LanguageServer\Impl\Microsoft.R.LanguageServer.csproj
-set DOTNET_PUBLISH
