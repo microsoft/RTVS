4646dabb10b053c4a0d6596433054beeb33f5d6e
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-06-08 16:00:11 -0700

    Packaging

--- src/Host/Broker/Impl/Start/ProgramBase.cs
+++ src/Host/Broker/Impl/Start/ProgramBase.cs
@@ -4,13 +4,16 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.R.Host.Broker.Logging;
 
 namespace Microsoft.R.Host.Broker.Start {
     public abstract class ProgramBase {
         public static IWebHost WebHost { get; protected set; }
         protected static void MainEntry<T>(string[] args) where T : Startup {
             var cm = new Configurator(args);
+
+            //while (!System.Diagnostics.Debugger.IsAttached) {
+            //    System.Threading.Thread.Sleep(1000);
+            //}
 
             WebHost = cm
                 .ConfigureWebHost()
